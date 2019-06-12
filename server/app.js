0c0eb214d5866be2738c1c0b7982865a2cc01e4e
Tony Tang <tonytang215@hotmail.com>
2019-06-12 12:29:14 +1000

    correctly installed dev typescript, added start script

--- server/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 8000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(port, function () {
-    console.log('Server listening on port ' + port);
-});
