fca30035527d865f50dc8f1b3ed9262fe25c5f45
Tony Tang <tonytang215@hotmail.com>
2019-06-14 10:48:05 +1000

    #5 Separated app into app and server

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
-const port = 8000;
 
+/**
+ * Express configuration
+ */ 
+app.set("port", process.env.PORT || 3000);
+
+/**
+ * Express routes
+ */
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
-  console.log('Server listening on port ' + port)
-});
+export default app;
