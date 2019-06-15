e301254712bf89535ea2b1274e8114905c53453a
Tony Tang <tonytang215@hotmail.com>
2019-06-15 19:36:04 +1000

    #5 added indexController

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express();
+import express from 'express'
+import * as indexController from './controllers/index'
+
+const app = express()
 
 /**
  * Express configuration
- */ 
-app.set("port", process.env.PORT || 3000);
+ */
+app.set('port', process.env.PORT || 3000)
 
 /**
  * Express routes
  */
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.get('/', indexController.index)
 
-export default app;
+export default app
