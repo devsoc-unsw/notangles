268a589f1c5908b8573387ba50e690a4044dd8c5
shanil <shanildesilva@gmail.com>
2019-06-19 11:12:49 +1000

    connected to main database

--- server/db.ts
+++ server/db.ts
@@ -2,12 +2,14 @@
 
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
+const config = require('./config.ts').config
 
 // URL for database
-const url = "mongodb+srv://user:user@testdb-zaztm.mongodb.net/test?retryWrites=true&w=majority"
+const env = process.env.NODE_ENV || 'development'
+const url = config[env].url
 
 // DB Name
-const dbName = "Notangles"
+const dbName = 'Notangles'
 
 // Create a the MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true })
@@ -15,14 +17,13 @@ const client = new MongoClient(url, { useNewUrlParser: true })
 //Connecting to server
 client.connect(function(err) {
   assert.equal(null, err)
-  console.log("Connected successfully to server")
+  console.log('Connected successfully to server')
   const db = client.db(dbName)
-  const col = db.collection("Courses")
-  //col.insertOne({id:"COMP1511"})
-  col.deleteOne({id:"COMP1511"}, function(err,document) {
-  	if (err) throw err
-  	console.log(document.id)
+  const col = db.collection('Courses')
+  col.insertOne({ id: 'COMP1511' })
+  col.findOne({ id: 'COMP1511' }, function(err, document) {
+    if (err) throw err
+    console.log(document.id)
   })
-  client.close();
+  client.close()
 })
-
