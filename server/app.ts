1d6d099da28d3d3ae92a915c0e36358dd0e2ccfb
shanil <shanildesilva@gmail.com>
2019-06-26 10:06:22 +1000

    Started changing code to asynch

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
+import db from './db_api'
+
 const express = require('express')
-const app = express();
-const port = 8000;
+const app = express()
+const port = 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-});
+})
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port)
-});
+})
--- server/db.ts
+++ server/db.ts
@@ -1,29 +1,47 @@
 //Command to run datase: mongod --dbpath=./data
 
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
-const config = require('./config.ts').config
+//const MongoClient = require('mongodb').MongoClient
+//const assert = require('assert')
+//const config = require('./config.ts').config
 
 // URL for database
-const env = process.env.NODE_ENV || 'development'
-const url = config[env].url
+//const env = process.env.NODE_ENV || 'test'
+//const url = config[env].url
 
 // DB Name
-const dbName = 'Notangles'
+//const dbName = 'Notangles'
 
 // Create a the MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true })
+//const client = new MongoClient(url, { useNewUrlParser: true })
+
+import Database from './db_api'
+
+// const add = () => {
+
+// }
+
+const main = async () => {
+  console.log('hai')
+  await Database.db_create({ id: 'COMP1591' })
+  console.log('bai')
+}
+
+main().then(() => {
+  Database.disconnect()
+  process.exit()
+})
+
+//dbm.db_del('1')
+// var doc = dbm.db_read({ id: 'COMP1511' })
+// console.log(doc)
 
 //Connecting to server
-client.connect(function(err) {
+/*client.connect(function(err) {
   assert.equal(null, err)
   console.log('Connected successfully to server')
-  const db = client.db(dbName)
-  const col = db.collection('Courses')
-  col.insertOne({ id: 'COMP1511' })
-  col.findOne({ id: 'COMP1511' }, function(err, document) {
-    if (err) throw err
-    console.log(document.id)
-  })
+  //const db = client.db(dbName)
+  //const col = db.collection('Courses')
+  //col.insertOne({ id: 'COMP1511' })
+  //col.deleteOne({ id: 'COMP1511' })
   client.close()
-})
+})*/
