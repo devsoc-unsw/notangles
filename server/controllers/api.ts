2880c3084e5b656cc39121b4e9bd9f3aa94e9b89
Tony Tang <tonytang215@hotmail.com>
2019-06-28 11:14:32 +1000

    explicitly defined params in interface

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -1,8 +1,13 @@
 import { Response, Request } from 'express'
 
+interface IGetCourseParams {
+  courseId: string
+}
+
 /**
- * GET /api/course/:courseId
+ * GET /api/course/:courseId/:cooursecontent
  */
 export const getCourse = (req: Request, res: Response) => {
-  res.send(req.params)
+  const params: IGetCourseParams = req.params
+  res.send(params.courseId)
 }
