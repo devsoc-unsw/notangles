91fd5946c82bb7bc579d8be4dec72c4844270dd1
Tony Tang <tonytang215@hotmail.com>
2019-06-28 10:33:47 +1000

    #8 const instead of let

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -3,6 +3,6 @@ import { Response, Request } from 'express'
 /**
  * GET /api/course/:courseId
  */
-export let getCourse = (req: Request, res: Response) => {
+export const getCourse = (req: Request, res: Response) => {
   res.send(req.params)
 }
