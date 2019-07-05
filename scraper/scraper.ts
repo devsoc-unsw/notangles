b29b7daf7ff5fd9d1dd89a1c1ad97a43b7dac5dd
AD9000 <damle.atharv5@gmail.com>
2019-07-05 20:39:43 +1000

    Setup complete. Navigate to timetable page

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,11 +1,26 @@
 import * as puppeteer from 'puppeteer'
+const chalk = require("chalk");
 
-const main = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://example.com')
-  await page.screenshot({ path: 'example.png' })
-  await browser.close()
-}
+const error = chalk.bold.red;
+const success = chalk.keyword("green");
 
-main()
+(async() => {
+    try
+    {
+        // Launch the browser. Headless mode.
+        var browser = await puppeteer.launch();
+        var page = await browser.newPage();
+        
+        // Go to the timetable page
+        await page.goto('http://timetable.unsw.edu.au/2019/subjectSearch.html', { waitUntil: 'networkidle2' });
+        
+        // Close the browser.
+        await browser.close();
+        console.log(success("Browser closed"));
+    }
+    catch (err) {
+        console.log(error(err));
+        await browser.close();
+        console.log(error("Browser closed"));
+    }
+})();
