df2d770ee2f4979d2d8b91b87ac7d33a0714564b
Atharv Damle <damle.atharv16@gmail.com>
2019-07-17 13:06:55 +1000

    added in the dependencies

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,15 +1,17 @@
 import * as puppeteer from 'puppeteer'
+
+// import * as .. does not work for chalk, so require.
 const chalk = require("chalk");
 
 const error = chalk.bold.red;
-const success = chalk.keyword("green");
+const success = chalk.green;
 
 (async() => {
+    // Launch the browser. Headless mode = true by default
+    const browser = await puppeteer.launch();
     try
     {
-        // Launch the browser. Headless mode.
-        var browser = await puppeteer.launch();
-        var page = await browser.newPage();
+        let page = await browser.newPage();
         
         // Go to the timetable page
         await page.goto('http://timetable.unsw.edu.au/2019/subjectSearch.html', { waitUntil: 'networkidle2' });
@@ -19,6 +21,7 @@ const success = chalk.keyword("green");
         console.log(success("Browser closed"));
     }
     catch (err) {
+        // log error and close browser.
         console.log(error(err));
         await browser.close();
         console.log(error("Browser closed"));
