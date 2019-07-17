c54f6e8991d0436e8db0f7a8853378973504cc48
AD9000 <damle.atharv16@gmail.com>
2019-07-17 13:34:49 +1000

    set up eslint and prettier

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,29 +1,28 @@
-import * as puppeteer from 'puppeteer'
+import * as puppeteer from 'puppeteer';
 
 // import * as .. does not work for chalk, so require.
-const chalk = require("chalk");
+const chalk = require('chalk');
 
 const error = chalk.bold.red;
 const success = chalk.green;
+(async () => {
+  // Launch the browser. Headless mode = true by default
+  const browser = await puppeteer.launch();
+  try {
+    let page = await browser.newPage();
 
-(async() => {
-    // Launch the browser. Headless mode = true by default
-    const browser = await puppeteer.launch();
-    try
-    {
-        let page = await browser.newPage();
-        
-        // Go to the timetable page
-        await page.goto('http://timetable.unsw.edu.au/2019/subjectSearch.html', { waitUntil: 'networkidle2' });
-        
-        // Close the browser.
-        await browser.close();
-        console.log(success("Browser closed"));
-    }
-    catch (err) {
-        // log error and close browser.
-        console.log(error(err));
-        await browser.close();
-        console.log(error("Browser closed"));
-    }
+    // Go to the timetable page
+    await page.goto('http://timetable.unsw.edu.au/2019/subjectSearch.html', {
+      waitUntil: 'networkidle2'
+    });
+
+    // Close the browser.
+    await browser.close();
+    console.log(success('Browser closed'));
+  } catch (err) {
+    // log error and close browser.
+    console.log(error(err));
+    await browser.close();
+    console.log(error('Browser closed'));
+  }
 })();
