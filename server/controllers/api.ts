6db4ae8b7c5b31053d95b96e0df603a09a601aa0
Tony Tang <tonytang215@hotmail.com>
2019-07-17 12:12:26 +1000

    added basic exception handling in API

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -11,5 +11,11 @@ interface IGetCourseParams {
 export const getCourse = async (req: Request, res: Response) => {
   const params: IGetCourseParams = req.params
   const course = await db.dbRead(params.courseId)
+
+  if (!course) {
+    console.error(`dbRead returned null course. params: ${params.courseId}`)
+    res.status(400).send('Invalid courseId param')
+  }
+
   res.send(course)
 }
