cf7d391cc7b83dbd6dc025ac05373c16539c52eb
Tony Tang <tonytang215@hotmail.com>
2019-07-19 17:05:59 +1000

    Refactored API in anticipation for addition of termId variable in #21

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -1,21 +1,27 @@
 import { Response, Request } from 'express'
+import * as util from 'util'
+
 import db from '../dbApi'
 
 interface IGetCourseParams {
+  termId: string
   courseId: string
 }
 
 /**
- * GET /api/course/:courseId/
+ * GET /api/terms/:termId/courses/:courseId/
  */
 export const getCourse = async (req: Request, res: Response) => {
   const params: IGetCourseParams = req.params
-  const course = await db.dbRead(params.courseId)
+  const course = await db.dbRead(params.termId, params.courseId)
 
-  if (!course) {
-    console.error(`dbRead returned null course. params: ${params.courseId}`)
+  if (course) {
+    res.send(course)
+  } else {
+    console.error(
+      'dbRead returned null course. params: ' +
+        util.inspect(params, { showHidden: false, depth: null })
+    )
     res.status(400).send('Invalid courseId param')
   }
-
-  res.send(course)
 }
