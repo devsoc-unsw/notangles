f8032fe12064d002738a77ff3e258d102e2dc5c2
PeterGanZW <zhuowei@gmail.com>
2019-07-26 11:18:09 +1000

    initial implementation of fetch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,6 +21,24 @@ const App: React.FC = () => {
   const handleChange = (e: any) => {
     setValue(e)
   }
+
+  fetch('/api/terms/T2-2019/courses/COMP1521/')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  ).catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+  
   return (
     <div>
       <h2>Notangles</h2>
