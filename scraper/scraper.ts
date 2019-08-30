80b680d292beefe46dcb8e81da34db7d31f86f1c
AD9000 <damle.atharv16@gmail.com>
2019-08-30 19:42:13 +1000

    Removed scraper runner script

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -478,16 +478,4 @@ const timetableScraper = async () => {
   }
 };
 
-(async () => {
-  console.time('scraper');
-  await timetableScraper();
-  console.timeEnd('scraper');
-  const used = process.memoryUsage();
-  for (let key in used) {
-    console.log(
-      `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
-    );
-  }
-})();
-
 export { timetableScraper };
