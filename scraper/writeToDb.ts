42b460df107c2c9044d72a192b25fe70db6814f8
shanil <shanildesilva@gmail.com>
2019-09-11 13:08:39 +1000

    some style improvements

--- scraper/writeToDb.ts
+++ scraper/writeToDb.ts
@@ -1,12 +1,13 @@
 import { timetableScraper } from './scraper'
 import Database from '../server/dbApi'
-import db from '../server/dbApi'
 
 // Assuming data is like the db schema
-timetableScraper().then(function(data) {
-  for (let term of data) {
-    const termColName = term.name
-    for (let course of term) {
+timetableScraper().then(function(terms) {
+  for (const term of terms) {
+    // Get the name of the term collection
+    const termColName: string = term.name
+
+    for (const course of term) {
       if (Database.dbRead(termColName, course.courseCode) === null) {
         Database.dbAdd(termColName, course)
       } else {
