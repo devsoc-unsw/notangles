fdc4468c328bcbd62579b4c04ea3de7a49af4fe5
shanil <shanildesilva@gmail.com>
2019-10-25 10:38:50 +1100

    refactored the dbname to be an argument in the ddatabase api

--- scraper/writeToDb.ts
+++ scraper/writeToDb.ts
@@ -6,15 +6,15 @@ import { TimetableData, Course, Class, Time } from './interfaces'
 const main = async () => {
   //writing the data to the database
   let date = new Date()
-  Database.setDbname(date.getFullYear().toString(10))
+  const year = date.getFullYear().toString(10)
   const terms = await timetableScraper(date.getFullYear())
   for (const [termName, term] of Object.entries(terms.timetableData)) {
     for (const course of term) {
-      const ret = await Database.dbRead(termName, course.courseCode)
+      const ret = await Database.dbRead(year, termName, course.courseCode)
       if (ret === null) {
-        await Database.dbAdd(termName, course)
+        await Database.dbAdd(year, termName, course)
       } else {
-        await Database.dbUpdate(termName, course.courseCode, course)
+        await Database.dbUpdate(year, termName, course.courseCode, course)
       }
     }
   }
--- server/dbApi.ts
+++ server/dbApi.ts
@@ -5,7 +5,6 @@ const url = config.database
 
 class Database {
   private client: MongoClient | undefined
-  private dbName = 'Notangles'
 
   connect = async () => {
     const client = new MongoClient(url, { useNewUrlParser: true })
@@ -18,42 +17,43 @@ class Database {
     }
   }
 
-  getDb = async (): Promise<Db> => {
+  getDb = async (dbName: string): Promise<Db> => {
     if (!this.client) await this.connect()
-    const db = this.client.db(this.dbName)
+    const db = this.client.db(dbName)
     return db
   }
 
-  getCollection = async (termColName: string) => {
-    const db = await this.getDb()
+  getCollection = async (dbName: string, termColName: string) => {
+    const db = await this.getDb(dbName)
     return db.collection(termColName)
   }
 
-  dbAdd = async (termColName: string, doc) => {
-    const col = await this.getCollection(termColName)
+  dbAdd = async (dbName: string, termColName: string, doc) => {
+    const col = await this.getCollection(dbName, termColName)
     await col.insertOne(doc)
   }
 
-  dbRead = async (termColName: string, courseCode: string) => {
-    const col = await this.getCollection(termColName)
+  dbRead = async (dbName: string, termColName: string, courseCode: string) => {
+    const col = await this.getCollection(dbName, termColName)
     const doc = await col.findOne({ courseCode })
     return doc
   }
 
-  dbUpdate = async (termColName: string, courseCode: string, doc) => {
-    const col = await this.getCollection(termColName)
+  dbUpdate = async (
+    dbName: string,
+    termColName: string,
+    courseCode: string,
+    doc
+  ) => {
+    const col = await this.getCollection(dbName, termColName)
     try {
       await col.updateOne({ courseCode }, { $set: doc })
     } catch (e) {}
   }
 
-  dbDel = async (termColName: string, courseCode: string) => {
-    const col = await this.getCollection(termColName)
+  dbDel = async (dbName: string, termColName: string, courseCode: string) => {
+    const col = await this.getCollection(dbName, termColName)
     await col.deleteOne({ courseCode })
-  }
-
-  setDbname = (name: string) => {
-    this.dbName = name
   }
 }
 
