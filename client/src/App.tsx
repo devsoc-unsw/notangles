08c457687978b301eb4724f564cc0e6821700951
AD9000 <damle.atharv16@gmail.com>
2019-11-08 11:41:17 +1100

    displays top 10 courses received from the db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Select from 'react-select'
 
-import TimeTable from './components/timetable'
+import TimeTable, { Course } from './components/timetable'
 import Navbar from './components/navbar'
 
 import styled from 'styled-components'
@@ -9,6 +9,12 @@ import styled from 'styled-components'
 interface CourseOption {
   value: string
   label: string
+}
+
+interface CourseOverview {
+  courseCode: string
+  name: string
+  id: string
 }
 
 const options: CourseOption[] = [
@@ -40,22 +46,58 @@ const StyledSelect = styled(Select)`
 
 const App: React.FC = () => {
   const [value, setValue] = React.useState<CourseOption>()
+
+  // List of courses
+  const [coursesList, setCoursesList] = React.useState<CourseOverview[]>()
+  const [options, setOptions] = React.useState<CourseOption[]>()
   const handleChange = (e: any) => {
+    console.log(e)
     setValue(e)
   }
 
-  React.useEffect(() => {
-    fetch('http://localhost:3001/api/terms/2019-T1/courses/COMP1511/')
-      .then(function(response) {
-        return response.json()
+  // Once -> when the app is rendered
+  useEffect(() => {
+    fetch('http://localhost:3001/api/terms/2019-T1/courses')
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        setCoursesList(result)
       })
-      .then(function(myJson) {
-        console.log(JSON.stringify(myJson))
-      })
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err)
-      })
+    console.log(options)
+    // setOptions(options)
   }, [])
+
+  useEffect(() => {
+    console.log('something')
+    if (coursesList) {
+      setOptions(
+        coursesList
+          .filter((value, index) => {
+            return index < 10
+          })
+          .map(course => {
+            const option: CourseOption = {
+              value: course.courseCode,
+              label: course.courseCode + ' - ' + course.name,
+            }
+            return option
+          })
+      )
+    }
+  }, [coursesList])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/api/terms/2019-T1/courses/COMP1511/')
+  //     .then(function(response) {
+  //       return response.json()
+  //     })
+  //     .then(function(myJson) {
+  //       console.log(JSON.stringify(myJson))
+  //     })
+  //     .catch(function(err) {
+  //       console.log('Fetch Error :-S', err)
+  //     })
+  // }, [])
 
   return (
     <div className="App">
