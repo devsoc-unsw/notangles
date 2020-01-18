bd76e2286984eb9471c104e1030732eefb4bae36
Atharv Damle <damle.atharv16@gmail.com>
2020-01-18 15:46:23 +1100

    Update scraper/PageScraper.ts
    Co-Authored-By: Tony Tang <tonytang.dev@gmail.com>

--- scraper/PageScraper.ts
+++ scraper/PageScraper.ts
@@ -23,7 +23,7 @@ interface getDataUrlsParams {
 
 /**
  * Gets all the urls in the data class on page: page,
- * given regex: regex.
+ * Gets all the urls in the data class on page matching regex
  * Each url will have the prefix: base.
  * @param { puppeteer.Page } page Page to scrape urls from
  * @param { string } base string each url has to be prefixed with
