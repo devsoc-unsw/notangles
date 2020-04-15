217a0f8415b54f179577546184c2280f6c38e1a0
Max Wo <maxwo@protonmail.com>
2020-04-15 14:47:42 +1200

    Make server differentiate between deployment environments using environemnt variables

--- server/src/config.ts
+++ server/src/config.ts
@@ -12,18 +12,18 @@ interface Config {
 export const allConfig: Record<Env, Config> = {
   [Env.DEV]: {
     // database: secret.dev,
-    database: 'mongodb://database:27017/',
+    database: 'mongodb://localhost:27017/',
     scraper: 'mongodb://localhost:27017',
   },
 
   [Env.STAGING]: {
-    database: 'mongodb://database:27017/',
+    database: 'mongodb://database.notangles-db:27017/',
     scraper: 'mongodb://localhost:27017',
   },
 
   [Env.PROD]: {
-    database: 'mongodb://database:27017/',
-    scraper: 'mongodb://localhost:27017',
+    database: 'mongodb://database.notangles-db:27017/',
+    scraper: 'mongodb://database.notangles-db:27017',
   },
 }
 export const config: Config = allConfig[process.env.NODE_ENV || Env.DEV]
