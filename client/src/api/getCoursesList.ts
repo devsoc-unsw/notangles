1b64b6843aa93e34341a0dc78afd2be6e4f3d74f
Max Wo <maxwo@protonmail.com>
2020-04-18 17:30:26 +1200

    Allow client to connect to deployed server

--- client/src/api/getCoursesList.ts
+++ client/src/api/getCoursesList.ts
@@ -1,6 +1,6 @@
 import { CoursesList } from '../interfaces/CourseOverview'
 
-const API_URL = 'http://localhost:3001/api'
+const API_URL = process.env.REACT_APP_ENVIRONMENT === 'development' ? 'http://localhost:3001/api' : 'https://notangles-server.csesoc.unsw.edu.au/api'
 
 /**
  * Fetches a list of course objects, where each course object contains 
