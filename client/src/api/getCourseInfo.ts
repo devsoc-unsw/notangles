1ce9fe89b4c41949a4c88b3b3ef43c5de80458c9
Luke Fisk-Lennon <z5169800@unsw.edu.au>
2020-04-20 23:02:44 +1000

    temporarily hardcoding server address

--- client/src/api/getCourseInfo.ts
+++ client/src/api/getCourseInfo.ts
@@ -1,16 +1,16 @@
 import { DbCourse, dbCourseToCourseData } from '../interfaces/DbCourse'
 import { CourseData } from '../interfaces/CourseData'
 
-const API_URL = 'http://localhost:3001/api'
+const API_URL = 'https://notangles-server.csesoc.unsw.edu.au/api'
 
 /**
  * Fetches the information of a specified course
- * 
+ *
  * @param year The year that the course is offered in
  * @param term The term that the course is offered in
  * @param courseCode The code of the course to fetch
  * @return A promise containing the information of the course that is offered in the specified year and term
- * 
+ *
  * @example
  * const selectedCourseClasses = await getCourseInfo('2019', 'T3', 'COMP1511')
  */
--- client/src/api/getCoursesList.ts
+++ client/src/api/getCoursesList.ts
@@ -1,15 +1,15 @@
 import { CoursesList } from '../interfaces/CourseOverview'
 
-const API_URL = 'http://localhost:3001/api'
+const API_URL = 'https://notangles-server.csesoc.unsw.edu.au/api'
 
 /**
- * Fetches a list of course objects, where each course object contains 
+ * Fetches a list of course objects, where each course object contains
  * the course id, the course code, and course name
- * 
+ *
  * @param year The year that the courses are offered in
  * @param term The term that the courses are offered in
  * @return A promise containing the list of course objects offered in the specified year and term
- * 
+ *
  * @example
  * const coursesList = await getCoursesList('2020', 'T1')
  */
