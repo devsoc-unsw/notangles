b3c232540c756c487937d15e3ee1522d8f1a4492
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2020-04-29 21:04:26 +1000

    Feature/51 material timetable (#85)
    * Apply material theme to timetable
    
    * Fixed comment
    
    * Change dropzone opacity
    
    * Removed unnecessary import
    
    * Wider timetable, inventory classes rounded, code/activity on one line
    
    * Fix warnings, layout improvement
    
     - Upgrade styled-components and @types/styled-components
     - More responsive layout
     - Fixed nav bar
     - Fix for warnings being caused by props passed down to DOM
     - Centering class text
     - Flat dropzone style
     - Shorten class location
    
    * Prevent class text wrapping
    
    * Increase timetable min-height
    
    * bring back margin at bottom
    
    * Fix buggy dnd
    
    * More readable layout padding

--- client/src/hooks/useColorMapper.ts
+++ client/src/hooks/useColorMapper.ts
@@ -1,41 +1,43 @@
 import { useState, useEffect } from 'react'
-import { colours } from '../constants/timetable'
+import { colors } from '../constants/timetable'
+
+const defaultColor = colors[colors.length - 1]
 
 /**
  * Assigns a unique color, chosen from a set of fixed colors, to each course code
- * 
+ *
  * @param courseCodes An array containing course codes
  * @return An object that maps a color to each course code
- * 
+ *
  * @example
  * const assignedColors = useColorMapper(selectedCourses.map(course => course.courseCode))
  */
 export const useColorMapper = (courseCodes: string[]): Record<string, string> => {
-  const [assignedColours, setAssignedColours] = useState<
+  const [assignedColors, setAssignedColors] = useState<
     Record<string, string>
   >({})
 
   useEffect(() => {
     const takenColors = new Set<string>()
-    const newAssignedColours: Record<string, string> = {}
-    
+    const newAssignedColors: Record<string, string> = {}
+
     courseCodes.forEach(item => {
       const color =
-        item in assignedColours
-          ? assignedColours[item]
-          : colours.find(c => !takenColors.has(c))
-      newAssignedColours[item] = color || 'orange'
+        item in assignedColors
+          ? assignedColors[item]
+          : colors.find(c => !takenColors.has(c))
+      newAssignedColors[item] = color || defaultColor
       if (color) {
         takenColors.add(color)
       }
     })
 
     if (
-      JSON.stringify(assignedColours) !== JSON.stringify(newAssignedColours)
+      JSON.stringify(assignedColors) !== JSON.stringify(newAssignedColors)
     ) {
-      setAssignedColours(newAssignedColours)
+      setAssignedColors(newAssignedColors)
     }
   }, [courseCodes])
 
-  return assignedColours
+  return assignedColors
 }
