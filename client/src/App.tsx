27af0bed158c5b37bd8068adf676524a11802000
itsViggo <vigo@192-168-1-108.tpgi.com.au>
2020-05-01 22:56:51 +1000

    fixed bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -73,10 +73,11 @@ const App: FunctionComponent = () => {
   }, [isDarkMode]);
 
   const handleSelectCourse = async (e: CourseOption) => {
-    const selectedCourseClasses = await getCourseInfo('2020', 'T2', e.value);
-
-    if (selectedCourseClasses) {
-      setSelectedCourses([...selectedCourses, selectedCourseClasses]);
+    if (selectedCourses.filter(course => course.courseCode == e.value).length == 0) {
+      const selectedCourseClasses = await getCourseInfo('2020', 'T2', e.value);
+      if (selectedCourseClasses) {
+        setSelectedCourses([...selectedCourses, selectedCourseClasses]);
+      }
     }
   };
 
