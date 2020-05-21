5073cdc7188700bd176a302262eec379f56974d2
Max Wo <maxwo@protonmail.com>
2020-05-21 22:08:44 +1200

    Add .editorconfig

--- common/jest.config.js
+++ common/jest.config.js
@@ -1,19 +1,7 @@
 const NO_COVERAGE = process.env.NO_COVERAGE === '1';
-const CLEAR_CONSOLE = process.env.CLEAR_CONSOLE === '1';
-
-const notice = () => console.log('Using Jest config from `jest.config.js`');
-
-if (CLEAR_CONSOLE) {
-  require('clear')();
-  console.log();
-  notice();
-  console.log('Clearing console due to CLEAR_CONSOLE=1');
-} else {
-  notice();
-}
 
 if (NO_COVERAGE) {
-  console.log('Coverage not collected due to NO_COVERAGE=1');
+  console.info('Coverage not collected due to NO_COVERAGE=1');
 }
 
 console.log('Type checking is disabled during Jest for performance reasons, use `jest typecheck` when necessary.');
