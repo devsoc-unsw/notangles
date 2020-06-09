1521f8b7915210c26d9c2d565aef56549209f795
ecl1ipse <shanildesilva@gmail.com>
2020-06-09 15:58:13 +1000

    need to work more on clash.ts. I believe it is currently bugged and it does not contain calculations for minutes

--- client/src/automation/clash.ts
+++ client/src/automation/clash.ts
@@ -4,7 +4,6 @@ export const clash = (time1: ClassTime, time2: ClassTime): boolean => {
     if (time1.day != time2.day) {
         return false;
     }
-
     const start_hour1 = Number(time1.start.substring(0,2))
     const start_hour2 = Number(time2.start.substring(0,2))
     const start_min1 = Number(time1.start.substring(3,5))
@@ -15,22 +14,21 @@ export const clash = (time1: ClassTime, time2: ClassTime): boolean => {
     const end_min1 = Number(time1.end.substring(3,5))
     const end_min2 = Number(time2.end.substring(3,5))
 
-    var clashing: boolean = false
-    if (start_hour1 <= end_hour2 && start_hour1 >= start_hour2) {
-        clashing = true
+    if (start_hour2 > start_hour1 && start_hour2 < end_hour1) {
+        return true
     }
 
-    if (end_hour1 <= end_hour2 && end_hour1 >= start_hour2) {
-        clashing = true
+    if (start_hour1 > start_hour2 && start_hour1 < end_hour2) {
+        return true
     }
 
-    if (start_hour2 <= end_hour1 && start_hour2 >= start_hour1) {
-        clashing = true
+    if (end_hour2 > start_hour1 && end_hour2 < end_hour1) {
+        return true
     }
 
-    if (end_hour2 <= end_hour1 && end_hour2 >= start_hour1) {
-        clashing = true
+    if (end_hour1 > start_hour2 && end_hour1 < end_hour2) {
+        return true
     }
 
-    return clashing
+    return false;
 }
