6c7b0e44330ef9ecef3684eaf2289e8970aa190c
esyw <50993997+esyw@users.noreply.github.com>
2020-06-09 23:47:51 +1000

    add some colours from material ui palette (#111)

--- client/src/constants/timetable.ts
+++ client/src/constants/timetable.ts
@@ -1,9 +1,13 @@
 export const colors: string[] = [
-  '#3a76f8', // temporary
-  '#00796b', // green
-  '#aa00ff', // purple
-  '#3f51b5', // blue
-  '#bc5100', // red
+  '#009689', // dark green
+  '#9E28AE', // violet
+  '#FF5725', // red
+  '#683BB5', // purple
+  '#EB1E62', // pink
+  '#00BCD4', // turqoise
+  '#3E51B3', // dark blue
+  '#FF981A', // orange
+  '#8AC352', // light green
 ];
 
 export const days: string[] = [
