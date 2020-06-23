4ca4a22c40d868e146d631e396d6a2bdb9557c24
Esther Wong <ew@Esthers-MacBook-Air.local>
2020-06-23 15:15:44 +1000

    change grid width

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -129,7 +129,7 @@ const App: FunctionComponent = () => {
           <ContentWrapper>
             <Content>
               <Grid container spacing={2}>
-                <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={9}>
                   <SelectWrapper>
                     <CourseSelect
                       selectedCourses={selectedCourses}
@@ -139,7 +139,7 @@ const App: FunctionComponent = () => {
                     />
                   </SelectWrapper>
                 </Grid>
-                <Grid item xs={12} md={4}>
+                <Grid item xs={12} md={3}>
                   <Autotimetabler isDarkMode={isDarkMode} />
                 </Grid>
               </Grid>
