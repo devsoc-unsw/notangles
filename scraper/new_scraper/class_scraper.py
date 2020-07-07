a7e08a59a34f3b167da458aa61f8c8ac978b2589
Michael Hopkins <hopko682@hotmail.com>
2020-07-07 16:29:47 +1000

    completed class scraper functionality

--- scraper/new_scraper/class_scraper.py
+++ scraper/new_scraper/class_scraper.py
@@ -1,35 +1,93 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 import re
 
-BASE_URL = "http://timetable.unsw.edu.au/2020/COMP1511.html"
+BASEURL = "http://timetable.unsw.edu.au/2020/COMP1511.html"
 
-def get_class_info(soup):
-    sessions = []
-    refined_data = {}
+def getTimetable(soup, this_class):
+    timetable = {}
+    rows = soup.find_all('tr')
+    classList = []
+    keys = []
+    for i in range(0, len(rows)):
+        classOffering = []
+        children = rows[i].findChildren()
+        if len(children) > 1:
+            for child in children:
+                classOffering.append(child.getText())
+        
+            classList.append(classOffering)
 
+    return classList
+
+def getClassLinks(soup):
+    summary_tables = []
+    anchorTags = []
+    terms = {}
     minor_headings = soup.find_all('td', {'class': 'classSearchSectionHeading'})
-
     for heading in minor_headings:
-        section = heading.get_text()
-        if re.search("Detail", section):
-            sessions.append(heading)
+        section = heading.getText()
+        if re.search("SUMMARY OF", section):
+            summary_tables.append(heading)
 
-    for session in sessions:
-        parent_table = session.parent.find_next('table')
-        class_forms = parent_table.find_all('td', {'class': 'formBody'})
-        for class_form in class_forms:
-            rows = class_form.find_all('tr')
-            for row in rows:
-                URL_data = row.find_all('td', {'class': ['label', 'data']})
-                for data_point in URL_data:
-                    line = data_point.get_text()
-                    print(line)
-            print('-----------------------')
+    # get the anchor tag labels for each class
+    for summary_table in summary_tables:
+        parent_table = summary_table.parent.findNext('table')
+        term_summaries = parent_table.find_all('td', {'class': 'formBody'})
+        for term_summary in term_summaries:
+            low = term_summary.find_all('tr', {'class' : 'rowLowlight'})
+            high = term_summary.find_all('tr', {'class' : 'rowHighlight'})
+            entries = low + high
 
-data = requests.get(BASE_URL)
+            for entry in entries:
+                cells = entry.find_all('td', {'class' : 'data'})
+                anchorTags.append((cells[0].find('a')['href']).replace('#', ''))
+
+    # get the class data
+    classesHTML = []
+    for anchor in anchorTags:
+        class_table = soup.find('a', {'name': anchor})
+        class_table = class_table.parent.parent.parent
+        classRows = class_table.find_all('tr')
+    
+        this_class = {}
+        for row in classRows:
+            labels = row.find_all('td', {'class' : 'label'})
+            
+            for label in labels:
+                sibling = label.find_next_sibling()
+                if sibling:
+                    label = label.getText()
+                    sibling = sibling.getText()
+                    this_class[label] = sibling
+                          
+            timetable = row.find('td', {'class': 'formBody'})
+            if timetable:
+                tables = []
+                table = getTimetable(timetable, this_class)
+                if len(table) > 1:
+                    times = {}
+                    for i in range(1, len(table)):
+                        for j in range(len(table[i])):
+                            times[table[0][j]] = table[i][j]
+
+                        if times:
+                            tables.append(times)
+
+                    if tables:
+                        this_class['Time Table'] = tables
+
+        if this_class:
+            if this_class['Teaching Period'] not in terms:
+                terms[this_class['Teaching Period']] = []
+            terms[this_class['Teaching Period']].append(this_class)
+    
+    return terms
+
+data = requests.get(BASEURL)
 data = data.text
 
 soup = BeautifulSoup(data, 'html.parser')
 
-get_class_info(soup)
+getClassLinks(soup)
