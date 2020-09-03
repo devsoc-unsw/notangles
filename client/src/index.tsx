9c02f713e4f69a3bb9f4776573d345e78e38d8e4
Max Wo <maxwo@protonmail.com>
2020-09-03 11:49:16 +1000

    Track page views on home page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ReactGA from 'react-ga';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-176748714-1', {
+  debug: process.env.NODE_ENV === 'development',
+});
+
+ReactGA.pageview(window.location.pathname);
 
 const Root: FunctionComponent = () => (
   <App />
