19d55090aabf9040df062f695d70f5628c271240
Esther Wong <ew@Esthers-MacBook-Air.local>
2020-09-13 14:30:21 +1000

    add friend drawer component

---
+++ client/src/components/Friends.tsx
@@ -0,0 +1,198 @@
+import React from 'react';
+import {
+  makeStyles, Theme, createStyles,
+} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+// import IconButton from '@material-ui/core/IconButton';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+// const useStyles = makeStyles((theme: Theme) => createStyles({
+//   root: {
+//     display: 'flex',
+//   },
+//   appBar: {
+//     transition: theme.transitions.create(['margin', 'width'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//   },
+//   appBarShift: {
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     marginLeft: drawerWidth,
+//     transition: theme.transitions.create(['margin', 'width'], {
+//       easing: theme.transitions.easing.easeOut,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   hide: {
+//     display: 'none',
+//   },
+//   drawer: {
+//     width: drawerWidth,
+//     flexShrink: 0,
+//   },
+//   drawerPaper: {
+//     width: drawerWidth,
+//   },
+//   drawerHeader: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     padding: theme.spacing(0, 1),
+//     // necessary for content to be below app bar
+//     ...theme.mixins.toolbar,
+//     justifyContent: 'flex-end',
+//   },
+//   content: {
+//     flexGrow: 1,
+//     padding: theme.spacing(3),
+//     transition: theme.transitions.create('margin', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     marginLeft: -drawerWidth,
+//   },
+//   contentShift: {
+//     transition: theme.transitions.create('margin', {
+//       easing: theme.transitions.easing.easeOut,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//     marginLeft: 0,
+//   },
+// }));
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    marginTop: '80px',
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+
+// interface FriendsListItemProps {
+// }
+
+// const FriendListItem: React.FC<FriendsListItemProps> = ({
+const FriendListItem: React.FC<any> = () => (
+
+  <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+    </ListItemAvatar>
+    <ListItemText
+      primary="Friendy McFriendFace"
+      secondary={(
+        <>
+          <Typography
+            component="span"
+            variant="body2"
+            color="textPrimary"
+          >
+            Ali Connors
+          </Typography>
+          {" — I'll be in your neighborhood doing errands this…"}
+        </>
+      )}
+    />
+  </ListItem>
+
+);
+
+
+interface FriendsProps {
+  isFriendsListOpen: boolean
+}
+
+
+const FriendsDrawer: React.FC<FriendsProps> = ({
+  isFriendsListOpen,
+}) => {
+// export default function FriendsDrawer() {
+  const classes = useStyles();
+  // const theme = useTheme();
+
+
+  // const handleDrawerClose = () => {
+  //   setOpen(false);
+  // };
+
+  // <div className={classes.drawerHeader}> ***
+
+  // TODO map props
+  // {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+  //     <ListItem button key={text}>
+  //     <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+  //     <ListItemText primary={text} />
+  //     </ListItem>
+  // ))}
+  return (
+
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={isFriendsListOpen}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerContainer}>
+        <List>
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+          <FriendListItem />
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </Drawer>
+
+  );
+};
+
+export default FriendsDrawer;
