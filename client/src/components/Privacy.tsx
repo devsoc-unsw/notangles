964717a685e1e16a3d5a57979a8ce52be07650e2
Max Wo <maxwo@protonmail.com>
2020-10-31 15:37:09 +1100

    Set up routing (#140)

---
+++ client/src/components/Privacy.tsx
@@ -0,0 +1,116 @@
+import React, { FunctionComponent } from 'react';
+
+const Privacy: FunctionComponent = () => (
+  <div>
+    <h1>Application privacy statement</h1>
+
+    <p>
+      This privacy statement (“Privacy Statement”) applies to the treatment of
+      personally identifiable information submitted by, or otherwise obtained from,
+      you in connection with the associated application (“Application”). The
+      Application is provided by Notangles (and may be provided by Notangles on
+      behalf of a Notangles licensor or partner (“Application Partner”). By using or
+      otherwise accessing the Application, you acknowledge that you accept the
+      practices and policies outlined in this Privacy Statement.
+    </p>
+
+    <h2>What personal information does Notangles collect?</h2>
+    <p>We collect the following types of information from our users:</p>
+    <p>Personal Information You Provide to Us:</p>
+    <p>
+      We may receive and store any information you submit to the Application (or
+      otherwise authorize us to obtain – such as, from (for example) your Facebook
+      account). The types of personal information collected may include your
+      username, email address, profile picture, friends that are also using the
+      Application, and any other information necessary for us to provide the
+      Application services.
+    </p>
+
+    <h2>How does Notangles use the information it collects?</h2>
+    <p>
+      Notangles uses the information described in this Privacy Statement (i)
+      internally, to analyze, develop and improve its products and services, and
+      (ii) as set forth below in the “Will Notangles share any of the personal
+      information it receives” section below.
+    </p>
+
+    <h2>Will Notangles share any of the personal information it receives?</h2>
+    <p>
+      Personal information about our users is an integral part of our business. We
+      neither rent nor sell your personal information to anyone.
+    </p>
+    <p>
+      Protection of Notangles and Others: We may release personal information when
+      we believe in good faith that release is necessary to comply with the law;
+      enforce or apply our conditions of use and other agreements; or protect the
+      rights, property, or safety of Notangles, our employees, our users, or others.
+      This includes exchanging information with other companies and organizations
+      for fraud protection and credit risk reduction.
+    </p>
+    <p>
+      With Your Consent: Except as set forth above, you will be notified when your
+      personal information may be shared with third parties, and will be able to
+      prevent the sharing of this information.
+    </p>
+
+    <h2>Conditions of use.</h2>
+    <p>
+      If you decide to use or otherwise access the Application, your use/access and
+      any possible dispute over privacy is subject to this Privacy Statement and our
+      Terms of Use, including limitations on damages, arbitration of disputes, and
+      application of California state law.
+    </p>
+
+    <h2>Third party applications/websites.</h2>
+    <p>
+      The Application may permit you to link to other applications or websites. Such
+      third party applications/websites are not under Notangles’s control, and such
+      links do not constitute an endorsement by Notangles of those other
+      applications/websites or the services offered through them. The privacy and
+      security practices of such third party application/websites linked to the
+      Application are not covered by this Privacy Statement, and Notangles is not
+      responsible for the privacy or security practices or the content of such
+      websites.
+    </p>
+
+    <h2>What personal information can I access?</h2>
+    <p>
+      Notangles allows you to access the following information about you for the
+      purpose of viewing, and in certain situations, updating that information. This
+      list may change in the event the Application changes.
+    </p>
+    <ul>
+      <li>Account and user profile information</li>
+      <li>User e-mail address, if applicable</li>
+      <li>Facebook profile information, if applicable</li>
+      <li>User preferences</li>
+      <li>Application specific data</li>
+    </ul>
+
+    <h2>Changes to this privacy statement.</h2>
+    <p>
+      Notangles may amend this Privacy Statement from time to time. Use of
+      information we collect now is subject to the Privacy Statement in effect at
+      the time such information is used. If we make changes in the way we use
+      personal information, we will notify you by posting an announcement on our
+      Site or sending you an email. Users are bound by any changes to the Privacy
+      Statement when he or she uses or otherwise accesses the Application after such
+      changes have been first posted.
+    </p>
+
+    <h2>Questions or concerns.</h2>
+    <p>
+      If you have any questions or concerns regarding privacy on our Website, please
+      send us a detailed message at projects@csesoc.org.au. We will make every
+      effort to resolve your concerns.
+    </p>
+    <p>
+      Effective Date: 11
+      <sup>th</sup>
+      {' '}
+      October, 2020
+    </p>
+  </div>
+);
+
+export default Privacy;
