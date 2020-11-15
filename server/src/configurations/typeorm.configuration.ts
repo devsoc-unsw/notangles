4a240aa5a79abc854e226db79142f9bcb7852df6
maxwowo <maxwo@protonmail.com>
2020-11-15 13:37:33 +1100

    Use Postgres

--- server/src/configurations/typeorm.configuration.ts
+++ server/src/configurations/typeorm.configuration.ts
@@ -8,13 +8,13 @@ import { EnvironmentService } from '../modules/environment/environment.service';
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   private readonly CONFIGURATION: Readonly<TypeOrmModuleOptions> = {
     autoLoadEntities: true,
-    database: this.configService.get<string>('TYPEORM_DATABASE', ''),
+    database: this.configService.get<string>('TYPEORM_DATABASE', 'notangles'),
     host: this.configService.get<string>('TYPEORM_HOST', 'localhost'),
-    password: this.configService.get<string>('TYPEORM_PASSWORD', ''),
-    port: this.configService.get<number>('TYPEORM_PORT', 27017),
-    synchronize: this.configService.get<boolean>('TYPEORM_SYNCHRONIZE', false),
-    type: 'mongodb',
-    username: this.configService.get<string>('TYPEORM_USERNAME', ''),
+    password: this.configService.get<string>('TYPEORM_PASSWORD', 'notangles'),
+    port: this.configService.get<number>('TYPEORM_PORT', 5432),
+    synchronize: this.configService.get<boolean>('TYPEORM_SYNCHRONIZE', true),
+    type: 'postgres',
+    username: this.configService.get<string>('TYPEORM_USERNAME', 'notangles'),
   };
 
   private readonly TEST_CONFIGURATION: Readonly<TypeOrmModuleOptions> = {
