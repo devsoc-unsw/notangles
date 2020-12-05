21d210038af201222b27e1fee5631d4f3b20bf4e
ecl1ipse <shanildesilva@gmail.com>
2020-12-05 23:19:31 +1100

    autoTT working

--- server/src/automation/autoTT.ts
+++ server/src/automation/autoTT.ts
@@ -1,15 +1,9 @@
 import {
-  CourseData,
-  ClassPeriod,
-  filterOutClasses,
-  ClassData,
-  ClassTime,
   Activity,
 } from '@notangles/common'
-import { clash } from './clash'
 import Database from '../database'
 import { dbReadParams } from '../database'
-import { dbCourseToCourseData } from './DbCourse'
+import { exit } from 'process'
 
 interface DbCourse {
   courseCode: string
@@ -21,6 +15,8 @@ interface DbClass {
   activity: string
   times: DbTimes[]
   courseEnrolment: DbCourseEnrolment
+  term: string
+  classID: string
 }
 
 interface DbCourseEnrolment {
@@ -45,6 +41,13 @@ interface autoCourse {
   exclude: Activity[]
 }
 
+interface sortClass {
+  id: string
+  code: string
+  time: DbTimes[]
+  activity: string
+}
+
 export interface autoCourses {
   courses: autoCourse[]
   term: string
@@ -52,128 +55,190 @@ export interface autoCourses {
   criteria: {}
 }
 
-interface sortClass {
-  time: ClassTime
-  id: string
-}
 
-interface activityObject {
-  code: string
-  activity: string
-  classes: sortClass[]
-}
+const autoTT = async (courses: autoCourses) => {
+  const dbCourses : DbCourse[] = []
 
-const dbReadToSort = (course: DbCourse) => {
-  const activities = []
-  course.classes.forEach((c) => {
-    if (!activities.includes(c.activity)) {
-      activities.push(c.activity)
-    }
-  })
-
-  const activityObjects: activityObject[] = []
-  activities.forEach((activity) => {
-    const act: activityObject = {
-      code: course.courseCode,
-      activity: activity,
-      classes: [],
-    }
-    activityObjects.push(act)
-  })
-
-  course.classes.forEach((c) => {
-    activityObjects.forEach((activityObject) => {})
-  })
-}
-
-const autoInit = async (courses: autoCourses) => {
-  const ttCourses: ClassData[] = []
+  // not using a foreach loop as async function calls cannot be made in a foreach loop
+  // Get more information from the db and change the datatype to sortCourse
   for (const course of courses['courses']) {
     const args: dbReadParams = {
-      dbName: courses['year'].toString(10),
-      termColName: courses['term'],
-      courseCode: courses['code'],
+      dbName: courses.year.toString(),
+      termColName: courses.term,
+      courseCode: course.code,
     }
-    ttCourses.push(await Database.dbRead(args))
-  }
-  console.log(ttCourses)
-}
-
-export const autoTT = (classes: ClassData[], calc: Function) => {
-  let maxScore: number = -1
-  let index: number = 0
-  let bestTT: ClassPeriod[] = []
-  if (classes.length === 0) {
-    return []
+    try {
+      dbCourses.push(await Database.dbRead(args))
+    } catch (error) {
+      console.log("error")
+      exit(1)
+    }
   }
 
-  classes.forEach((classSlot) => {
-    let tt: ClassPeriod[] = []
-    classSlot.periods.forEach((period) => {
-      tt.push(period)
+  const classDict : Record<string, sortClass[]> = {}
+  dbCourses.forEach(course => {
+    const sortClasses : sortClass[] = []
+    course.classes.forEach(clas => {
+      let exclude = false
+      courses.courses.forEach(c => {
+        if (c.code == course.courseCode) {
+          c.exclude.forEach(act => {
+            if (clas.activity == act) {
+              exclude = true
+            }
+          })
+        }
+      })
+
+
+      if (exclude == false) {
+        const dictString = course.courseCode + "-" + clas.activity
+        if (classDict[dictString] === undefined) {
+          classDict[dictString] = []
+        }
+
+        const sortClass : sortClass = {
+          id : clas.classID,
+          code : course.courseCode,
+          time: clas.times,
+          activity : clas.activity,
+        }
+        
+        classDict[dictString].push(sortClass)
+      }
     })
-    tt = fillTT(index, tt, calc, classes)
-    let score = calc(tt)
+  })
+
+  //console.log(classDict)
+
+  // find best tt
+  let maxScore : number = -1
+  let index : number = 0
+  let bestTT : sortClass[] = []
+
+  const key : string = Object.keys(classDict)[index]
+  classDict[key].forEach(clas => {
+    let newTT : sortClass[] = []
+    newTT.push(clas)
+    newTT = fillTT(classDict, newTT, index + 1, courses.criteria)
+    let score = calc(newTT, courses.criteria)
     if (score > maxScore) {
-      bestTT = tt.slice(0)
+      bestTT = newTT.slice(0)
       maxScore = score
     }
   })
+  bestTT.forEach(c => {
+    console.log("Class")
+    console.log(c)
+  })
+}
 
+const fillTT = (classDict : Record<string, sortClass[]>, TT : sortClass[], index : number, criteria : {}) => {
+  if (index >= Object.keys(classDict).length) {
+    return TT
+  }
+
+  let maxScore : number = -1
+  let bestTT : sortClass[] = []
+  const key : string = Object.keys(classDict)[index]
+  classDict[key].forEach(clas => {
+    let newTT : sortClass[] = TT.slice(0)
+
+    let clashing = false
+    newTT.forEach(t => {
+      if (clash(clas.time, t.time) == true) {
+        clashing = true
+      }
+    })
+
+    if (clashing == false) {
+      newTT.push(clas)
+      newTT = fillTT(classDict, newTT, index + 1, criteria)
+      let score = calc(newTT, criteria)
+      if (score > maxScore) {
+        bestTT = newTT.slice(0)
+        maxScore = score
+      }
+    }
+  })
   return bestTT
 }
 
-const fillTT = (
-  index: number,
-  tt: ClassPeriod[],
-  calc: Function,
-  classes: ClassData[]
-) => {
-  index += 1
-  if (index >= classes.length) {
-    return tt
-  }
-  let maxScore: number = -1
-  let bestTT: ClassPeriod[] = []
-  classes.forEach((classSlot) => {
-    let clashing: boolean = false
-    classSlot.periods.forEach((p1) => {
-      tt.forEach((p2) => {
-        if (clash(p1.time, p2.time) === true) {
+export const clash = (time1 : DbTimes[], time2 : DbTimes[]) => {
+  let clashing = false
+  //const rangePat = /\d+-\d/g
+  time1.forEach(t1 => {
+    time2.forEach(t2 => {
+      if (t1.day == t2.day) {
+        if (t1.time.start >= t2.time.start && t1.time.start < t2.time.end) {
           clashing = true
+        }
+      
+        if (t1.time.end > t2.time.start && t1.time.end <= t2.time.end) {
+          clashing = true
+        }
+      
+        if (t2.time.start >= t1.time.start && t2.time.start < t1.time.end) {
+          clashing = true
+        }
+      
+        if (t2.time.end > t1.time.start && t2.time.end <= t1.time.end) {
+          clashing = true
+        }
+
+        // check weeks
+
+        if (clashing == true) {
+          return clashing
+        }
+      }
+    })
+  })
+
+  return clashing
+}
+
+const calc = (TT : sortClass[], criteria : {}) : number => {
+  let score = 0
+  if (criteria["daysAtUni"] != undefined) {
+    const days : Record<string, boolean> = {}
+    TT.forEach(act => {
+      act.time.forEach(t => {
+        if (days[t.day] == undefined) {
+          days[t.day] = true
         }
       })
     })
 
-    if (clashing === false) {
-      let newTT = tt.slice(0)
-      classSlot.periods.forEach((period) => {
-        newTT.push(period)
-      })
+    let count = 0
+    Object.keys(days).forEach(day => {
+      count++
+    })
 
-      newTT = fillTT(index, newTT, calc, classes)
-      let score = calc(newTT)
-
-      if (score > maxScore) {
-        maxScore = score
-        bestTT = newTT
-      }
+    if (criteria["daysAtUni"] < 0) {
+      score += (7 - count) * (criteria["daysAtUni"] * -1)
+    } else {
+      score += (count) * criteria["daysAtUni"]
     }
-  })
+  }
 
-  return bestTT
+  return score
 }
 
 const request: autoCourses = {
   courses: [
     {
-      code: 'MATH1131',
-      exclude: ['Lecture'],
+      code: 'MATH1081',
+      exclude: [],
     },
+    {
+      code: 'COMP1511',
+      exclude: ["Lecture"],
+    }
   ],
   year: 2020,
-  term: 'T1',
+  term: 'T3',
   criteria: { daysAtUni: 10, napTime: 1 },
 }
 
-autoInit(request)
+autoTT(request)
