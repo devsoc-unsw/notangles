b2812e0ad711fe020f44dc9ce6797cebbe0b8831
OAK-02 <omarbdllh1@gmail.com>
2022-03-07 22:21:01 +1100

    made some changes for darkmode

--- client/src/constants/defaults.ts
+++ client/src/constants/defaults.ts
@@ -1,6 +1,6 @@
 const defaults: Record<string, any> = {
   is12HourMode: false,
-  isDarkMode: false,
+  isDarkMode: window.matchMedia('(prefers-color-scheme: dark)').matches,
   isSquareEdges: false,
   isHideFullClasses: false,
   isDefaultUnscheduled: false,
