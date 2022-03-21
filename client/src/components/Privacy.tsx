d4942593deabd38a89903fb6609e6b254b60010b
Mai Pham <angellapham240@gmail.com>
2022-03-21 14:12:33 +1100

    Moved privacy questions into a map instead of hard coding

--- client/src/components/Privacy.tsx
+++ client/src/components/Privacy.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Box } from '@material-ui/core';
 import styled from 'styled-components';
 import { contentPadding } from '../constants/theme';
