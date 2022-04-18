02b85614ee0d17d2f30f7d27e97b04b9fa8d6240
Martin <martin.knezevic1@gmail.com>
2022-04-18 22:58:46 +1000

    fix: Autotimetabling cleanup (#336)

--- server/proto-gen.sh
+++ server/proto-gen.sh
@@ -2,17 +2,23 @@
 
 PROTO_DEST=./src/proto
 
-# generate js codes via grpc-tools
-npx grpc_tools_node_protoc \
---js_out=import_style=commonjs,binary:./${PROTO_DEST} \
---grpc_out=./${PROTO_DEST} \
--I ./ \
-./*.proto
+# # generate js codes via grpc-tools
+# npx grpc_tools_node_protoc \
+# --js_out=import_style=commonjs,binary:./${PROTO_DEST} \
+# --grpc_out=./${PROTO_DEST} \
+# -I ./ \
+# ./*.proto
 
 
-# generate d.ts codes
-protoc \
---plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
---ts_out=./${PROTO_DEST} \
--I ./ \
-./*.proto
+# # generate d.ts codes
+# protoc \
+# --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+# --ts_out=./${PROTO_DEST} \
+# -I ./ \
+# ./*.proto
+
+
+
+
+npx grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./src/proto --grpc_out=./src/proto -I ./ ./autotimetabler.proto
+npx grpc_tools_node_protoc --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --ts_out=./src/proto -I ./ ./autotimetabler.proto
