565f855d8758f928b66327133aba1f68785bc512
Sijin Soon <sijin8828gmail.com>
2022-05-14 13:08:30 +1000

    changed width of the course select bar

--- client/src/components/controls/Controls.tsx
+++ client/src/components/controls/Controls.tsx
@@ -36,12 +36,12 @@ const HistoryWrapper = styled(Box)`
 const Controls: React.FC<ControlsProps> = ({ assignedColors, handleSelectClass, handleSelectCourse, handleRemoveCourse }) => {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={8}>
+      <Grid item xs={12} md={6.5}>
         <SelectWrapper>
           <CourseSelect assignedColors={assignedColors} handleSelect={handleSelectCourse} handleRemove={handleRemoveCourse} />
         </SelectWrapper>
       </Grid>
-      <Grid item container direction="row" alignItems="center" justifyContent="space-between" xs={12} md={4}>
+      <Grid item container direction="row" alignItems="center" justifyContent="space-between" xs={12} md={5.5}>
         <CustomEventsWrapper>
           <CustomEvents />
         </CustomEventsWrapper>
