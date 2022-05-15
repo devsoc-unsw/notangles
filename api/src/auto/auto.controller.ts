23ff307b13316c1252230b38478ffea736e04e7f
Martin <martin.knezevic1@gmail.com>
2022-05-15 14:56:18 +1000

    auto

--- api/src/auto/auto.controller.ts
+++ api/src/auto/auto.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { AutoService } from './auto.service';
 import { AutoDto } from './dtos/auto.dto';
 
 @Controller('auto')
 export class AutoController {
   @Post('/')
   async getAuto(@Body() body: AutoDto): Promise<any> {
-    return null;
+    return new AutoService().getAuto(body)
   }
 }
--- api/src/auto/auto.service.ts
+++ api/src/auto/auto.service.ts
@@ -7,33 +7,33 @@ import { TimetableConstraints } from './proto/autotimetabler_pb';
 
 @Injectable()
 export class AutoService {
-  // private readonly logger = new Logger(AutoService.name);
-  // async getAuto(data: AutoDto) {
-  //   const client = new AutoTimetablerClient(
-  //     config.auto,
-  //     credentials.createInsecure(),
-  //   );
-  //   const constraints = new TimetableConstraints();
-  //   constraints
-  //     .setStart(data.start)
-  //     .setEnd(data.end)
-  //     .setDays(data.days)
-  //     .setGap(data.gap)
-  //     .setMaxdays(data.maxdays);
-  //   data.periodInfoList.forEach((period) => {
-  //     const periodInfo = new TimetableConstraints.PeriodInfo();
-  //     periodInfo
-  //       .setPeriodsperclass(period.periodsPerClass)
-  //       .setPeriodtimesList(period.periodTimes)
-  //       .setDurationsList(period.durations);
-  //     constraints.addPeriodinfo(periodInfo);
-  //   });
-  //   client.findBestTimetable(constraints, (err, response) => {
-  //     if (err) {
-  //       this.logger.error(err);
-  //     } else {
-  //       this.logger.log(response);
-  //     }
-  //   });
-  // }
+  private readonly logger = new Logger(AutoService.name);
+  async getAuto(data: AutoDto) {
+    const client = new AutoTimetablerClient(
+      config.auto,
+      credentials.createInsecure(),
+    );
+    const constraints = new TimetableConstraints();
+    constraints
+      .setStart(data.start)
+      .setEnd(data.end)
+      .setDays(data.days)
+      .setGap(data.gap)
+      .setMaxdays(data.maxdays);
+    data.periodInfoList.forEach((period) => {
+      const periodInfo = new TimetableConstraints.PeriodInfo();
+      periodInfo
+        .setPeriodsperclass(period.periodsPerClass)
+        .setPeriodtimesList(period.periodTimes)
+        .setDurationsList(period.durations);
+      constraints.addPeriodinfo(periodInfo);
+    });
+    client.findBestTimetable(constraints, (err, response) => {
+      if (err) {
+        this.logger.error(err);
+      } else {
+        this.logger.log(response);
+      }
+    });
+  }
 }
