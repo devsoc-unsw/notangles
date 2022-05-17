1f2052901bdde3777e8c9be7e81c9ed74e9210e3
Max Phillips <max.phillips@nine.com.au>
2022-05-17 23:57:18 +1000

    Change default port to 3001 and send error message when no .env

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,16 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
+import { existsSync } from 'fs';
+
 async function bootstrap() {
+  // Check that a file called .env exists in the root of the project
+  // If not, then exit the process
+  if (!existsSync('.env')) {
+    console.error('No .env file found. Please create one.');
+    process.exit(1);
+  }
+
   // Setup NestJS
   const appOptions: NestApplicationOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
@@ -35,7 +44,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
