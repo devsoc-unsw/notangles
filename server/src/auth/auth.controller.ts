c87de1389be6868a8788824c9ae81ddcbc0c8f39
Max Phillips <max.phillips@nine.com.au>
2022-05-22 22:19:03 +1000

    Fix logout route

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Request, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 
 import { LoginGuard } from './login.guard';
-import { Issuer } from 'openid-client';
 
 @Controller()
 export class AuthController {
@@ -26,23 +25,9 @@ export class AuthController {
 
   @Get('/logout')
   async logout(@Request() req, @Res() res: Response) {
-    const id_token = req.user ? req.user.id_token : undefined;
-    req.logout();
-    req.session.destroy(async () => {
-      const TrustIssuer = await Issuer.discover(
-        `${process.env.OIDC_ISSUER_BASE_URL}/.well-known/openid-configuration`,
-      );
-      const end_session_endpoint = TrustIssuer.metadata.end_session_endpoint;
-      if (end_session_endpoint) {
-        res.redirect(
-          end_session_endpoint +
-            '?post_logout_redirect_uri=' +
-            process.env.OAUTH2_REDIRECT_URI +
-            (id_token ? '&id_token_hint=' + id_token : ''),
-        );
-      } else {
-        res.redirect('/');
-      }
+    req.session.destroy(() => {
+      res.clearCookie('connect.sid');
+      res.redirect('/');
     });
   }
 }
