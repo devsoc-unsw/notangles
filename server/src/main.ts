647c4c4d29adef80b191a9b21ef17001f745dc1f
Max Phillips <max.phillips@nine.com.au>
2022-05-22 21:58:10 +1000

    Fix resave setting in express-session

--- server/src/main.ts
+++ server/src/main.ts
@@ -23,13 +23,8 @@ async function bootstrap() {
   app.use(
     session({
       secret: process.env.SESSION_SECRET, // to sign session id
-      resave: false, // will default to false in near future: https://github.com/expressjs/session#resave
-      saveUninitialized: false, // will default to false in near future: https://github.com/expressjs/session#saveuninitialized
-      rolling: true, // keep session alive
-      cookie: {
-        maxAge: 30 * 60 * 1000, // session expires in 1hr, refreshed by `rolling: true` option.
-        httpOnly: true, // so that cookie can't be accessed via client-side script
-      },
+      resave: true,
+      saveUninitialized: false,
     }),
   );
   app.use(passport.initialize());
