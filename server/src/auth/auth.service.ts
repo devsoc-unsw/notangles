1ea773ece1e6fb79cc4f2903bd66682d3bbbf499
OAK-02 <omarbdllh1@gmail.com>
2022-06-16 10:54:37 +1000

    adding functionality of onboarding email

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,9 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument, UserInterface } from './schemas/user.schema';
 
-/**
- * TODO: Implement the logic for finding users in the database by their user ID here.
- */
 
 @Injectable()
 export class AuthService {
@@ -15,6 +12,7 @@ export class AuthService {
     let isCurrentUser = await this.getUser(userInfo.sub);
 
     if (isCurrentUser === null) {
+      console.log("NEW USER!");
       const newUser = {
         google_uid: userInfo.sub,
         firstname: userInfo.given_name,
@@ -25,11 +23,32 @@ export class AuthService {
 
       const userAdded = new this.userModel(newUser);
       userAdded.save();
+      await this.sendEmail(userInfo.email);
     }
   }
 
   async getUser(uidGiven: string): Promise<UserInterface | null> {
     const response = await this.userModel.find({ google_uid: uidGiven });
     return response.length !== 0 ? response.at(0) : null;
+  }
+
+  async sendEmail(userEmail: string): Promise<any> {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: userEmail,
+      from: 'notangles@csesoc.org.au',
+      subject: 'Notangles',
+      text: 'Welcome to Notangles.',
+      html: '<strong>Some message hear!</strong>',
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent!');
+    })
+    .catch((error) => {
+      console.error(error);
+    })
   }
 }
