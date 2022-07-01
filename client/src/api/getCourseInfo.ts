ec1e08e7bb20b8099d719b8627c575b97fc07f8c
gracexkan <grace.kan38@gmail.com>
2022-07-02 01:14:06 +1000

    merged overlapping period info

--- client/src/api/getCourseInfo.ts
+++ client/src/api/getCourseInfo.ts
@@ -3,6 +3,7 @@ import { DbCourse, dbCourseToCourseData } from '../interfaces/DbCourse';
 import { API_URL } from './config';
 import NetworkError from '../interfaces/NetworkError';
 import timeoutPromise from '../utils/timeoutPromise';
+import { parseJSON } from 'date-fns';
 
 /**
  * Fetches the information of a specified course
@@ -24,6 +25,34 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
       throw new NetworkError('Internal server error');
     }
     const json: DbCourse = await data.json();
+
+    json.classes.forEach((dbClassOne) => {
+      if ('times' in dbClassOne && dbClassOne.times.length > 1) {
+        for (let i = 0; i < dbClassOne.times.length - 1; i += 1) {
+          for (let j = i + 1; j < dbClassOne.times.length; j += 1) {
+            if (dbClassOne.times[i].day == dbClassOne.times[j].day
+              && dbClassOne.times[i].location == dbClassOne.times[j].location
+              && dbClassOne.times[i].time.start == dbClassOne.times[j].time.start
+              && dbClassOne.times[i].time.end == dbClassOne.times[j].time.end) {
+              let weeksOne = dbClassOne.times[i].weeks.split(/,|-/)
+              let weeksTwo = dbClassOne.times[j].weeks.split(/,|-/)
+              if (parseInt(weeksOne[weeksOne.length - 1]) == parseInt(weeksTwo[0]) - 1) {
+                let regex = new RegExp(weeksOne[weeksOne.length - 1] + "$", "g");
+                dbClassOne.times[i].weeks = dbClassOne.times[i].weeks.replace(regex, '') + dbClassOne.times[j].weeks
+              } else {
+                dbClassOne.times[i].weeks = dbClassOne.times[i].weeks + dbClassOne.times[j].weeks
+              }
+              dbClassOne.times.splice(dbClassOne.times.indexOf(dbClassOne.times[j]), 1)
+            }
+          }
+        }
+
+
+
+
+
+      }
+    });
     if (!json) {
       throw new NetworkError('Internal server error');
     }
