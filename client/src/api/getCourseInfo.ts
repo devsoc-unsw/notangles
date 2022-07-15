d50dc942f84dd1c262e1b97410f8f0ff1405e223
gracexkan <grace.kan38@gmail.com>
2022-07-15 23:27:19 +1000

    fixed code for merging course cards if they are the same but run on different weeks

--- client/src/api/getCourseInfo.ts
+++ client/src/api/getCourseInfo.ts
@@ -17,6 +17,41 @@ import { parseJSON } from 'date-fns';
  * @example
  * const selectedCourseClasses = await getCourseInfo('2019', 'T1', 'COMP1511')
  */
+
+const convertTimesToList = (dbClassWeeks: string, dbClassTimesList: number[]): number[] => {
+  for (let k = 0; k < dbClassWeeks.length; k++) {
+    if (dbClassWeeks[k] == '-') {
+      let maximum: number = parseInt(dbClassWeeks[k + 1])
+      if (k + 1 < dbClassWeeks.length - 1 && dbClassWeeks[k + 2] != ',' && dbClassWeeks[k + 2] != '-') {
+        maximum = parseInt(dbClassWeeks[k + 1] + dbClassWeeks[k + 2])
+      }
+      while (dbClassTimesList[dbClassTimesList.length - 1] < maximum - 1) {
+        dbClassTimesList.push(dbClassTimesList[dbClassTimesList.length - 1] + 1)
+      }
+    } else if (dbClassWeeks[k] != ',' && dbClassWeeks[k] != '-') {
+      if (k < dbClassWeeks.length - 1 && dbClassWeeks[k + 1] != ',' && dbClassWeeks[k + 1] != '-') {
+        dbClassTimesList.push(parseInt(dbClassWeeks[k] + dbClassWeeks[k + 1]))
+        k++;
+      } else {
+        dbClassTimesList.push(parseInt(dbClassWeeks[k]))
+      }
+    }
+  }
+  return dbClassTimesList
+}
+
+const sortUnique = (arr: number[]): number[] => {
+  if (arr.length === 0) return arr;
+  arr = arr.sort(function (a, b) { return a*1 - b*1; });
+  var ret = [arr[0]];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i-1] !== arr[i]) {
+      ret.push(arr[i]);
+    }
+  }
+  return ret;
+}
+
 const getCourseInfo = async (year: string, term: string, courseCode: CourseCode): Promise<CourseData> => {
   const baseURL = `${API_URL.timetable}/terms/${year}-${term}`;
   try {
@@ -25,24 +60,52 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
       throw new NetworkError('Internal server error');
     }
     const json: DbCourse = await data.json();
+    json.classes.forEach((dbClass) => {
+      if ('times' in dbClass && dbClass.times.length > 1) {
+        for (let i = 0; i < dbClass.times.length - 1; i += 1) {
+          for (let j = i + 1; j < dbClass.times.length; j += 1) {
+            let dbClassTimesOne = dbClass.times[i]
+            let dbClassTimesTwo = dbClass.times[j]
+            if (dbClassTimesOne.day == dbClassTimesTwo.day
+              && dbClassTimesOne.location == dbClassTimesTwo.location
+              && dbClassTimesOne.time.start == dbClassTimesTwo.time.start
+              && dbClassTimesOne.time.end == dbClassTimesTwo.time.end) {
+              let dbClassTimesList: number[] = []
 
-    json.classes.forEach((dbClassOne) => {
-      if ('times' in dbClassOne && dbClassOne.times.length > 1) {
-        for (let i = 0; i < dbClassOne.times.length - 1; i += 1) {
-          for (let j = i + 1; j < dbClassOne.times.length; j += 1) {
-            if (dbClassOne.times[i].day == dbClassOne.times[j].day
-              && dbClassOne.times[i].location == dbClassOne.times[j].location
-              && dbClassOne.times[i].time.start == dbClassOne.times[j].time.start
-              && dbClassOne.times[i].time.end == dbClassOne.times[j].time.end) {
-              let weeksOne = dbClassOne.times[i].weeks.split(/,|-/)
-              let weeksTwo = dbClassOne.times[j].weeks.split(/,|-/)
-              if (parseInt(weeksOne[weeksOne.length - 1]) == parseInt(weeksTwo[0]) - 1) {
-                let regex = new RegExp(weeksOne[weeksOne.length - 1] + "$", "g");
-                dbClassOne.times[i].weeks = dbClassOne.times[i].weeks.replace(regex, '') + dbClassOne.times[j].weeks
-              } else {
-                dbClassOne.times[i].weeks = dbClassOne.times[i].weeks + dbClassOne.times[j].weeks
+              dbClassTimesList = convertTimesToList(dbClassTimesOne.weeks, dbClassTimesList)
+              dbClassTimesList = convertTimesToList(dbClassTimesTwo.weeks, dbClassTimesList)
+
+              dbClassTimesList = sortUnique(dbClassTimesList)
+
+              let newWeeks: string = ''
+              let rangeStart = false
+
+              for (let k = 0; k < dbClassTimesList.length; k++) {
+                if (k == 0 || k == dbClassTimesList.length - 1) {
+                  newWeeks += dbClassTimesList[k]
+                } else if (rangeStart) {
+                  // add the start of the range
+                  newWeeks += dbClassTimesList[k]
+                  rangeStart = false
+                }
+
+                while (dbClassTimesList[k + 1] == dbClassTimesList[k] + 1) {
+                  // keep iterating until you reach the end of the range (numbers stop being consecutive)
+                  k++
+                }
+                if (!rangeStart) {
+                  // add the end of the range (last consecutive number)
+                  newWeeks += '-' + dbClassTimesList[k]
+                  if (k !== dbClassTimesList.length - 1) {
+                    // if this isn't the last week, we will need to add more weeks
+                    newWeeks += ','
+                  }
+                  // get ready to add the end of the range
+                  rangeStart = true
+                }
               }
-              dbClassOne.times.splice(dbClassOne.times.indexOf(dbClassOne.times[j]), 1)
+              dbClassTimesOne.weeks = newWeeks
+              dbClass.times.splice(dbClass.times.indexOf(dbClassTimesTwo), 1)
             }
           }
         }
