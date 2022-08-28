935e3ac91f6cc0ad4cf68e3fda986ddb88959cb9
Mun Joon Teo <teomunjoon101@gmail.com>
2022-08-28 15:14:38 +1000

    Put comments back

--- client/src/api/getCourseInfo.ts
+++ client/src/api/getCourseInfo.ts
@@ -7,43 +7,50 @@ import timeoutPromise from '../utils/timeoutPromise';
 import { API_URL } from './config';
 
 /**
- * Fetches the information of a specified course
+ * Converts a string representation of what weeks a class runs to an array
+ * e.g. "1-5,7-10" -> [1, 2, 3, 4, 5, 7, 8, 9, 10]
  *
- * @param year The year that the course is offered in
- * @param term The term that the course is offered in
- * @param courseCode The code of the course to fetch
- * @return A promise containing the information of the course that is offered in the
- * specified year and term
- *
- * @example
- * const selectedCourseClasses = await getCourseInfo('2019', 'T1', 'COMP1511')
+ * @param dbClassWeeks The weeks a class is running
+ * @param dbClassTimesList The output array
  */
-
 const convertTimesToList = (dbClassWeeks: string, dbClassTimesList: number[]) => {
   for (let k = 0; k < dbClassWeeks.length; k++) {
-    let times = dbClassWeeks.split(",")
-    times.map(time => {
-      if (time.includes("-")) {
+    let times = dbClassWeeks.split(',');
+    times.map((time) => {
+      if (time.includes('-')) {
         // convert ranges into numbers
-        let [min, max] = time.split("-")
+        let [min, max] = time.split('-');
         for (let j = parseInt(min); j < parseInt(max); j++) {
-          dbClassTimesList.push(j)
+          dbClassTimesList.push(j);
         }
       } else {
         // if not a range, add number to array directly
-        dbClassTimesList.push(parseInt(time))
+        dbClassTimesList.push(parseInt(time));
       }
-    })
-  };
-}
+    });
+  }
+};
 
+/**
+ * @param dbClassTimesOne The first class
+ * @param dbClassTimesTwo The second class
+ * @returns If the two classes are equivalent
+ */
 const classesAreEqual = (dbClassTimesOne: DbTimes, dbClassTimesTwo: DbTimes): boolean => {
-  return (dbClassTimesOne.day == dbClassTimesTwo.day &&
-  dbClassTimesOne.location == dbClassTimesTwo.location &&
-  dbClassTimesOne.time.start == dbClassTimesTwo.time.start &&
-  dbClassTimesOne.time.end == dbClassTimesTwo.time.end)
-}
+  return (
+    dbClassTimesOne.day == dbClassTimesTwo.day &&
+    dbClassTimesOne.location == dbClassTimesTwo.location &&
+    dbClassTimesOne.time.start == dbClassTimesTwo.time.start &&
+    dbClassTimesOne.time.end == dbClassTimesTwo.time.end
+  );
+};
 
+/**
+ * Sorts and removes duplicates from an array
+ *
+ * @param arr The array
+ * @returns The sorted array without duplicates
+ */
 const sortUnique = (arr: number[]): number[] => {
   if (arr.length === 0) return arr;
   arr = arr.sort((a, b) => {
@@ -62,10 +69,25 @@ const sortUnique = (arr: number[]): number[] => {
   return ret;
 };
 
+/**
+ * Fetches the information of a specified course
+ *
+ * @param year The year that the course is offered in
+ * @param term The term that the course is offered in
+ * @param courseCode The code of the course to fetch
+ * @return A promise containing the information of the course that is offered in the
+ * specified year and term
+ *
+ * @example
+ * const selectedCourseClasses = await getCourseInfo('2019', 'T1', 'COMP1511')
+ */
 const getCourseInfo = async (year: string, term: string, courseCode: CourseCode): Promise<CourseData> => {
   const baseURL = `${API_URL.timetable}/terms/${year}-${term}`;
   try {
     const data = await timeoutPromise(1000, fetch(`${baseURL}/courses/${courseCode}/`));
+
+    // Remove any leftover courses from localStorage if they are not offered in the current term
+    // which is why a 400 error is returned
     if (data.status === 400) {
       const selectedCourses = storage.get('selectedCourses');
       if (selectedCourses.includes(courseCode)) {
@@ -77,11 +99,16 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
     }
     const json: DbCourse = await data.json();
     json.classes.forEach((dbClass) => {
-      // loop through each class and search for another identical class
+      // Some courses split up a single class into two separate classes. e.g. CHEM1011 does it (as of 22T3)
+      // because one half of the course is taught by one lecturer and the other half is taught by another.
+      // This causes two cards to be generated for the same class which is not ideal, thus the following code
+      // consolidates the separate classes into one class.
+
       for (let i = 0; i < dbClass.times.length - 1; i += 1) {
         for (let j = i + 1; j < dbClass.times.length; j += 1) {
           let dbClassTimesOne = dbClass.times[i];
           let dbClassTimesTwo = dbClass.times[j];
+
           if (classesAreEqual(dbClassTimesOne, dbClassTimesTwo)) {
             let dbClassTimesList: number[] = [];
 
@@ -93,26 +120,31 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
             let newWeeks: string = '';
             let rangeStart = false;
 
+            // Convert the numerical representation of the weeks the classes are running back to a string
             for (let k = 0; k < dbClassTimesList.length; k++) {
               if (k == 0 || k == dbClassTimesList.length - 1) {
                 newWeeks += dbClassTimesList[k];
               } else if (rangeStart) {
-                // add the start of the range
+                // Add the start of the range
                 newWeeks += dbClassTimesList[k];
                 rangeStart = false;
               }
+
               while (dbClassTimesList[k + 1] == dbClassTimesList[k] + 1) {
-                // keep iterating until you reach the end of the range (numbers stop being consecutive)
+                // Keep iterating until you reach the end of the range (numbers stop being consecutive)
                 k++;
               }
+
               if (!rangeStart) {
-                // add the end of the range (last consecutive number)
+                // Add the end of the range (last consecutive number)
                 newWeeks += '-' + dbClassTimesList[k];
+
+                // If this isn't the last week, we will need to add more weeks
                 if (k !== dbClassTimesList.length - 1) {
-                  // if this isn't the last week, we will need to add more weeks
                   newWeeks += ',';
                 }
-                // get ready to add the end of the range
+
+                // Get ready to add the end of the range
                 rangeStart = true;
               }
             }
@@ -123,9 +155,8 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
       }
     });
 
-    if (!json) {
-      throw new NetworkError('Internal server error');
-    }
+    if (!json) throw new NetworkError('Internal server error');
+
     return dbCourseToCourseData(json);
   } catch (error) {
     throw new NetworkError('Could not connect to server');
@@ -133,3 +164,4 @@ const getCourseInfo = async (year: string, term: string, courseCode: CourseCode)
 };
 
 export default getCourseInfo;
+
