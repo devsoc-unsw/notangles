06b2df8bd9b9322bd3d60f6ee9f14b9a63e6d45e
Ray <raiyan.ahmed@gmail.com>
2022-09-21 22:15:14 +1000

    Removed the app controllers and providers with the 'hello' endpoints

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { SumModule } from './sum/sum.module';
-import { AutoModule } from './auto/auto.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
-import { FriendController } from './friend/friend.controller';
 import { FriendModule } from './friend/friend.module';
 
 @Module({
@@ -16,13 +12,12 @@ import { FriendModule } from './friend/friend.module';
     ConfigModule.forRoot(), // so that we can pull in config
     AuthModule,
     SumModule,
-    // AutoModule,
     MongooseModule.forRoot(`${process.env.DATABASE_URL}`),
     DatabaseModule,
     FriendModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
