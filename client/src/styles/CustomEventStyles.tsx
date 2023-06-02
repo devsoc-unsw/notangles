ed5c80948543e839efdeed761f0733d505b9df61
Sally Sun <sallyysun101@gmail.com>
2023-06-02 22:24:45 +1000

    Styling

--- client/src/styles/CustomEventStyles.tsx
+++ client/src/styles/CustomEventStyles.tsx
@@ -59,6 +59,7 @@ export const StyledMenu = styled((props: MenuProps) => (
     borderColor: alpha(theme.palette.grey[800], 0.85),
     borderWidth: 'thin',
     marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(2),
     minWidth: 180,
     color: theme.palette.grey[300],
     backgroundColor: alpha(theme.palette.grey[900], 0.9),
@@ -68,6 +69,12 @@ export const StyledMenu = styled((props: MenuProps) => (
         color: theme.palette.grey[300],
         marginRight: theme.spacing(1.5),
       },
+      // If you want a border on the hover as well as animation
+      // "&:hover": {
+        // borderRadius: 5,
+        // marginRight: theme.spacing(0.85),
+        // marginLeft: theme.spacing(0.85),
+      // },
       '&:active': {
         backgroundColor: alpha(
           theme.palette.primary.main,
