6355827052d759d14c2349a465249bbf4f4014ed
Ray <raiyan.ahmed@gmail.com>
2023-09-05 15:57:56 +1000

    prep to add end

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
   imports: [],
   controllers: [AppController],
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  app.setGlobalPrefix('api');
+  await app.listen(3003); // reminder to change it back to 3001
 }
 bootstrap();
