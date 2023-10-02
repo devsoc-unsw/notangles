297589fb88dc63f066888a1e5bd63fea1b3c86d1
Michael Siu <michaelsiu01@gmail.com>
2023-10-02 15:32:06 +1100

    fix: fix friend routes; start adding class validators to timetable DTO

--- server/src/friend/friend.controller.ts
+++ server/src/friend/friend.controller.ts
@@ -3,37 +3,38 @@ import { friendDto, friendRequestDto } from './dto';
 import { FriendService } from './friend.service';
 @Controller('friend')
 export class FriendController {
-  constructor (private friendService: FriendService) {}
+  constructor(private friendService: FriendService) {}
   @Get(':userId')
-  findAllFriends(@Param('userId') userId: string) { 
+  findAllFriends(@Param('userId') userId: string) {
     return this.friendService.findAllFriends(userId);
   }
 
-  
   @Post()
   friendUsers(@Body() friendDto: friendDto) {
-    return this.friendService.friendUsers(friendDto.senderId, friendDto.sendeeId);
+    return this.friendService.friendUsers(
+      friendDto.senderId,
+      friendDto.sendeeId,
+    );
   }
 
   @Delete()
   unfriendUsers(@Body() friendDTO: friendDto) {
-    return this.friendService.unfriendUsers(friendDTO.senderId, friendDTO.sendeeId);
+    return this.friendService.unfriendUsers(
+      friendDTO.senderId,
+      friendDTO.sendeeId,
+    );
   }
-  
 
-
-  @Post()
+  @Post('request')
   sendFriendRequest(@Body() friendDTO: friendDto) {
-    return this.friendService.sendFriendRequest(friendDTO.senderId, friendDTO.sendeeId);
+    return this.friendService.sendFriendRequest(
+      friendDTO.senderId,
+      friendDTO.sendeeId,
+    );
   }
-   
-  
-  @Delete()
+
+  @Delete('request')
   deleteFriendRequest(@Body() friendRequestDto: friendRequestDto) {
     return this.friendService.deleteFriendRequest(friendRequestDto.requestId);
   }
-  
-  
-
-
 }
--- server/src/user/dto/timetable.dto.ts
+++ server/src/user/dto/timetable.dto.ts
@@ -1,6 +1,14 @@
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class TimetableDto {
+  @IsString()
   timetableId: string; // Randomly generated on the backend
+
+  @IsArray()
+  @IsString({ each: true })
   selectedCourses: string[];
+
   events: EventDto[];
 }
 
