d57e9e0b7bd5b757a64603f05570fd47dabbeaae
Rayahhhmed <raiyan.ahmed@unsw.edu.au>
2023-10-26 12:42:11 +1100

    added friends and friendrequest relations for users

---
+++ server/prisma/seed.ts
@@ -0,0 +1,43 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+// initialize Prisma Client
+const prisma = new PrismaClient();
+const maximumZidNumber = 9e7;
+const minimumZidNumber = 1e7;
+
+const getZid = (): number => {
+  return Math.floor(Math.random() * (maximumZidNumber - minimumZidNumber + 1) + minimumZidNumber);
+}
+// TODO
+const generateTimetable = async () => {} 
+
+const generateUsers = async (numUsers: number, generateTimetablesForAllUsers? : boolean) => {
+  for (let i = 0; i < numUsers; i++) {
+    const zid = getZid().toString(); 
+    const user = await prisma.user.upsert({
+        where: { userId: zid},   
+        update: {},
+        create: {
+        userId: zid,
+        email: `z${zid}@unsw.edu.au`,
+      }
+    });
+    console.log(user);
+  }
+}
+
+async function main() {
+  generateUsers(2);
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
