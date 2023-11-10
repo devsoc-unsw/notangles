499b528b26817edffa6a86a35f7bfda6524e6da5
Sijin Soon <sijin8828gmail.com>
2023-11-10 18:23:47 +1100

    rm lines of unnecessary code

--- client/src/components/controls/CustomEvent.tsx
+++ client/src/components/controls/CustomEvent.tsx
@@ -279,7 +279,6 @@ const CustomEvent: React.FC = () => {
                 </div>
               </div>
               <div className="col-span-3 justify-self-end">
-                {/* <button type="button" className="flex justify-between gap-x-1.5 rounded-md bg-[#f8f8f8] px-3 py-2 text-md font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true"> */}
                 <ColorPicker
                   color={color}
                   setColor={setColor}
@@ -287,10 +286,6 @@ const CustomEvent: React.FC = () => {
                   handleOpenColorPicker={handleOpenColorPicker}
                   handleCloseColorPicker={handleCloseColorPicker}
                 />
-                {/* <svg className="my-auto h-5 w-5 text-gray-400 items-center" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                  </svg>
-                </button> */}
               </div>
             </div>
           </div>
