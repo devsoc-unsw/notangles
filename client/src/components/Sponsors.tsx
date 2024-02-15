2e6a340709dc1fd439730fb239f9f5c6f764623c
Jeremy Le <z5421623@ad.unsw.edu.au>
2024-02-16 00:46:44 +1100

    revamped logos display, adjusted size, positioning and refactored code

--- client/src/components/Sponsors.tsx
+++ client/src/components/Sponsors.tsx
@@ -1,32 +1,53 @@
-import { Grid } from '@mui/material';
-import { styled } from '@mui/system';
+import { Link, Stack } from '@mui/material';
 import { useContext } from 'react';
 
-import macquarie_light from '../assets/macquarie_logo.svg';
-import macquarie_dark from '../assets/macquarie_logo_white.svg';
-import tiktok_light from '../assets/tiktok_logo.svg';
-import tiktok_dark from '../assets/tiktok_logo_white.svg';
+import janeStreetLight from '../assets/sponsors/jane_street.svg';
+import janeStreetDark from '../assets/sponsors/jane_street_white.svg';
+import macquarieLight from '../assets/sponsors/macquarie_logo.svg';
+import macquarieDark from '../assets/sponsors/macquarie_logo_white.svg';
+import tiktokLight from '../assets/sponsors/tiktok_logo.svg';
+import tiktokDark from '../assets/sponsors/tiktok_logo_white.svg';
 import { AppContext } from '../context/AppContext';
-
-const SponsorImg = styled('img')`
-  display: block;
-  margin: 0px 20px 5px;
-  width: 25%;
-  border-radius: 2%;
-`;
 
 const Sponsors = () => {
   const { isDarkMode } = useContext(AppContext);
-  const tiktok = isDarkMode ? tiktok_dark : tiktok_light;
-  const macquarie = isDarkMode ? macquarie_dark : macquarie_light;
+
+  const sponsorData = [
+    {
+      name: 'Jane Street',
+      logo: isDarkMode ? janeStreetDark : janeStreetLight,
+      link: 'https://www.janestreet.com/',
+    },
+    {
+      name: 'TikTok',
+      logo: isDarkMode ? tiktokDark : tiktokLight,
+      link: 'https://www.tiktok.com',
+    },
+    {
+      name: 'Macquarie',
+      logo: isDarkMode ? macquarieDark : macquarieLight,
+      link: 'https://www.macquarie.com/au/en.html',
+    },
+  ];
 
   return (
     <div>
-      <h1 style={{ fontSize: '18px' }}>Our Sponsors</h1>
-      <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
-        <SponsorImg src={tiktok} alt="tiktok logo" />
-        <SponsorImg src={macquarie} alt="macquarie logo" />
-      </Grid>
+      <h1 style={{ fontSize: '14px' }}>Proudly Supported By</h1>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        direction={{ xs: 'column', lg: 'row' }}
+        marginY={2}
+        spacing={5}
+      >
+        {sponsorData.map((sponsor) => {
+          return (
+            <Link target="_blank" href={sponsor.link}>
+              <img src={sponsor.logo} alt={sponsor.name} width={200} />
+            </Link>
+          );
+        })}
+      </Stack>
     </div>
   );
 };
