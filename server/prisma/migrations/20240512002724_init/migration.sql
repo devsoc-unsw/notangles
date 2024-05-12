18f3808de8a23d698fc632c13098ff58bd0d3436
ray <raiyan.ahmed@gmail.com>
2024-05-13 09:28:34 +1000

    added example env files

---
+++ server/prisma/migrations/20240512002724_init/migration.sql
@@ -0,0 +1,112 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastLogin" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "firstname" TEXT,
+    "lastname" TEXT,
+    "email" TEXT NOT NULL,
+    "profileURL" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT,
+    "description" TEXT,
+    "colour" TEXT NOT NULL DEFAULT '#1F7E8C',
+    "day" TEXT NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+    "timetableId" TEXT,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" TEXT NOT NULL,
+    "classType" TEXT NOT NULL,
+    "courseName" TEXT,
+    "timetableId" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Timetable" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "selectedCourses" TEXT[],
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Timetable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Settings" (
+    "userId" TEXT NOT NULL,
+    "is12HourMode" BOOLEAN NOT NULL,
+    "isDarkMode" BOOLEAN NOT NULL,
+    "isSquareEdges" BOOLEAN NOT NULL,
+    "isHideFullClasses" BOOLEAN NOT NULL,
+    "isDefaultUnscheduled" BOOLEAN NOT NULL,
+    "isHideClassInfo" BOOLEAN NOT NULL,
+    "isSortAlphabetic" BOOLEAN NOT NULL,
+    "isShowOnlyOpenClasses" BOOLEAN NOT NULL,
+    "isHideExamClasses" BOOLEAN NOT NULL,
+    "isConvertToLocalTimezone" BOOLEAN NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_UserFriends" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Class_id_key" ON "Class"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Timetable_id_key" ON "Timetable"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Settings_userId_key" ON "Settings"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserFriends_AB_unique" ON "_UserFriends"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserFriends_B_index" ON "_UserFriends"("B");
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_timetableId_fkey" FOREIGN KEY ("timetableId") REFERENCES "Timetable"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_timetableId_fkey" FOREIGN KEY ("timetableId") REFERENCES "Timetable"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Timetable" ADD CONSTRAINT "Timetable_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Settings" ADD CONSTRAINT "Settings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFriends" ADD CONSTRAINT "_UserFriends_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFriends" ADD CONSTRAINT "_UserFriends_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
