01c62978a57d8b590fc96b7d7d6a792f9d05271a
ray <raiyan.ahmed@gmail.com>
2024-05-28 10:55:14 +1000

    removed the extra files

--- server/src/auto/auto.module 2.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AutoService } from './auto.service';
-import { AutoController } from './auto.controller';
-
-@Module({
-  controllers: [AutoController],
-  providers: [AutoService],
-})
-export class AutoModule {}
--- server/src/friend/friend.module 2.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FriendService } from './friend.service';
-import { FriendController } from './friend.controller';
-
-@Module({
-  providers: [FriendService],
-  controllers: [FriendController],
-})
-export class FriendModule {}
