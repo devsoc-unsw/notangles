5969eaf02fd638d0775718a589c9afae43b1b670
Shaam <shaamjevan.sj@gmail.com>
2024-06-04 11:06:22 +1000

    style: cleaned up inline styling

--- client/src/components/sidebar/CollapseButton.tsx
+++ client/src/components/sidebar/CollapseButton.tsx
@@ -3,28 +3,27 @@ import { IconButton, Tooltip } from '@mui/material';
 import { styled } from '@mui/system';
 import { BarsArrowDownIcon } from '@heroicons/react/24/solid';
 
-const StyledCollapseButton = styled(IconButton)`
-  border-radius: 8px;
-  color: ${({ theme }) => theme.palette.text.primary};
-`;
-
 interface CollapseButtonProps {
   collapsed: boolean;
   onClick: () => void;
   toolTipTitle: string;
 }
 
+const StyledCollapseButton = styled(IconButton)`
+  border-radius: 8px;
+  color: ${({ theme }) => theme.palette.text.primary};
+`;
+
+const StyledBarsArrowDownIcon = styled(BarsArrowDownIcon)<{ collapsed: boolean }>`
+  transform: ${({ collapsed }) => (collapsed ? 'rotate(270deg)' : 'rotate(90deg)')};
+`;
+
 const CollapseButton: React.FC<CollapseButtonProps> = ({ collapsed, onClick, toolTipTitle }) => {
   return (
     <>
       <Tooltip title={collapsed ? toolTipTitle : ''} placement="right">
         <StyledCollapseButton onClick={onClick}>
-          <BarsArrowDownIcon
-            width={28}
-            height={28}
-            style={{ transform: collapsed ? 'rotate(270deg)' : 'rotate(90deg)' }}
-            color="inherit"
-          />
+          <StyledBarsArrowDownIcon width={28} height={28} collapsed={collapsed} color="inherit" />
         </StyledCollapseButton>
       </Tooltip>
     </>
