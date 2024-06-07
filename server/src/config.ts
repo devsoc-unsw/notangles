3676a70f5152e094754f8577e161699f9cbe41b0
ray <raiyan.ahmed@gmail.com>
2024-06-07 21:46:17 +1000

    added more env vars to config

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,14 +5,20 @@ export enum Env {
 
 interface Config {
   auto: string;
+  client: string;
+  redirect_link: string;
 }
 
 export const API_CONFIG: Record<Env, Config> = {
   [Env.DEV]: {
     auto: 'localhost:50051',
+    client: 'http://localhost:5173',
+    redirect_link: 'http://localhost:5173',
   },
   [Env.PROD]: {
-    auto: `${process.env.AUTO_SERVER_HOSTNAME}:50051`,
+    auto: `${process.env.AUTO_SERVER_HOST_NAME}:${process.env.AUTO_SERVER_HOST_PORT}`,
+    client: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
+    redirect_link: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
   },
 };
 export const config: Config = API_CONFIG[process.env.NODE_ENV || Env.DEV];
