9b89237da01f63da0b448d47c438bab966385f0b
ray <raiyan.ahmed@gmail.com>
2024-06-12 15:14:22 +1000

    adding env files

--- server/src/config.ts
+++ server/src/config.ts
@@ -11,9 +11,9 @@ interface Config {
 
 export const API_CONFIG: Record<Env, Config> = {
   [Env.DEV]: {
-    auto: 'localhost:50051',
-    client: 'http://localhost:5173',
-    redirect_link: 'http://localhost:5173',
+    auto: `${process.env.AUTO_SERVER_HOST_NAME}:${process.env.AUTO_SERVER_HOST_PORT}`,
+    client: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
+    redirect_link: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
   },
   [Env.PROD]: {
     auto: `${process.env.AUTO_SERVER_HOST_NAME}:${process.env.AUTO_SERVER_HOST_PORT}`,
