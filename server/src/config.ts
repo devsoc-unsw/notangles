3b522a0bcddca5a2270754600cb597c8f2d53db4
ray <raiyan.ahmed@gmail.com>
2024-06-17 14:58:58 +1000

    removed config comments

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,30 +1,3 @@
-// export enum Env {
-//   DEV = 'dev',
-//   PROD = 'production',
-// }
-
-// interface Config {
-//   auto: string;
-//   client: string;
-//   redirect_link: string;
-// }
-
-// export const API_CONFIG: Record<Env, Config> = {
-//   [Env.DEV]: {
-//     auto: 'localhost:50051',
-//     client: 'http://localhost:5173',
-//     redirect_link: 'http://localhost:5173',
-//   },
-//   [Env.PROD]: {
-//     auto: process.env.AUTO_SERVER_HOST_NAME
-//       ? 'localhost:50051'
-//       : `${process.env.AUTO_SERVER_HOST_NAME}:${process.env.AUTO_SERVER_HOST_PORT}`,
-//     client: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
-//     redirect_link: `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}`,
-//   },
-// };
-// export const config: Config = API_CONFIG[process.env.NODE_ENV || Env.DEV];
-
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
