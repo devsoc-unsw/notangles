2436bb215627c6ba4c2b0e6d442a225142ea85b1
Michael Siu <michaelsiu01@gmail.com>
2024-07-22 16:00:53 +1000

    feat: add select component to choose calendar view type

--- client/src/components/controls/WeekSelector.tsx
+++ client/src/components/controls/WeekSelector.tsx
@@ -1,13 +1,14 @@
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
-import { Button, IconButton } from '@mui/material';
+import { Button, FormControl, IconButton, InputLabel, MenuItem, Select } from '@mui/material';
 import { useContext } from 'react';
 
 import { AppContext } from '../../context/AppContext';
 import { CourseContext } from '../../context/CourseContext';
+import { ViewType } from '../../interfaces/Calendar';
 
 const WeekSelector = () => {
   const { createdEvents, setCreatedEvents } = useContext(CourseContext);
-  const { currentDate, setCurrentDate } = useContext(AppContext);
+  const { currentDate, setCurrentDate, calendarView, setCalendarView } = useContext(AppContext);
 
   const handleBackClick = () => {
     const newDate = new Date(currentDate);
@@ -23,15 +24,25 @@ const WeekSelector = () => {
 
   return (
     <div>
-      <IconButton onClick={handleBackClick}>
-        <ChevronLeft />
-      </IconButton>
-      <Button variant="contained" color="primary" onClick={() => setCurrentDate(new Date())}>
-        Today
-      </Button>
-      <IconButton onClick={handleForwardClick}>
-        <ChevronRight />
-      </IconButton>
+      <div>
+        <IconButton onClick={handleBackClick}>
+          <ChevronLeft />
+        </IconButton>
+        <Button variant="contained" color="primary" onClick={() => setCurrentDate(new Date())}>
+          Today
+        </Button>
+        <IconButton onClick={handleForwardClick}>
+          <ChevronRight />
+        </IconButton>
+      </div>
+      <FormControl>
+        <InputLabel>View</InputLabel>
+        <Select value={calendarView} onChange={() => setCalendarView(calendarView)}>
+          <MenuItem value={ViewType.DAY}>Day</MenuItem>
+          <MenuItem value={ViewType.WEEK}>Week</MenuItem>
+          <MenuItem value={ViewType.MONTH}>Month</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
