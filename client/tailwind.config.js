9f708205b235e7645e89f1216da97d37ee089ba6
Dylan Zhang <z5478419@ad.unsw.edu.au>
2024-07-25 20:49:16 +1000

    added hovering animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        updown: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        updown: 'updown 1.3s ease-in-out infinite',
+      },
+    },
   },
   variants: {},
   plugins: [],
