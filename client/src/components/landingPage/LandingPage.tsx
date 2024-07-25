0b1c97f1d37ffaee35fe0cbf29d07b3d7db086e4
Dylan Zhang <z5478419@ad.unsw.edu.au>
2024-07-25 20:49:37 +1000

    added sponsor section

--- client/src/components/landingPage/LandingPage.tsx
+++ client/src/components/landingPage/LandingPage.tsx
@@ -1,8 +1,13 @@
 import HeroSection from "./HeroSection/heroSection";
+import SponsorsSection from "./SponsorsSection";
+import { FlipWords } from "./flip-words";
 
 const LandingPage = () => {
   return (
-    <HeroSection />
+    <div className="bg-blue-100">
+      <HeroSection />
+      <SponsorsSection />
+    </div>
   );
 };
 
