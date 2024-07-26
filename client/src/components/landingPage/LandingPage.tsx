926bb52b24031bc47678127bbe3426ee7bdb812a
Dylan Zhang <z5478419@ad.unsw.edu.au>
2024-07-26 17:19:54 +1000

    sync slight changes

--- client/src/components/landingPage/LandingPage.tsx
+++ client/src/components/landingPage/LandingPage.tsx
@@ -1,10 +1,9 @@
 import HeroSection from "./HeroSection/heroSection";
 import SponsorsSection from "./SponsorsSection";
-import { FlipWords } from "./flip-words";
 
 const LandingPage = () => {
   return (
-    <div className="bg-blue-100">
+    <div className="bg-blue-100 min-h-[3000px]">
       <HeroSection />
       <SponsorsSection />
     </div>
--- client/src/components/landingPage/SponsorsSection.tsx
+++ client/src/components/landingPage/SponsorsSection.tsx
@@ -2,20 +2,18 @@ import devsoc from '../../assets/devsoc.svg';
 import janeStreet from '../../assets/sponsors/jane_street_black.svg';
 import macquarie from '../../assets/sponsors/macquarie_logo_black.svg';
 import tiktok from '../../assets/sponsors/tiktok_logo_black.svg';
-import { MoreHoriz} from '@mui/icons-material';
 
 const SponsorsSection = () => {
   return (
-    <div className='flex items-center justify-center mt-44'>
+    <div className='flex items-center justify-center mt-00'>
       <div className='flex flex-col text-center items-center font-bold text-3xl'>
-        <p>Brought to you by.</p>
-        <img src={devsoc} className='h-28 pt-2 pb-2' />
-        <MoreHoriz fontSize='large' color='inherit' />
-        <p className='mt-8'>Our Sponsors</p>
+        <p>Brought to you by</p>
+        <img src={devsoc} className='h-24 pt-2 pb-2' />
+        <p className='mt-2'>Our Sponsors</p>
         <div className='flex pt-6'>
-          <img src={janeStreet} className='h-20' />
-          <img src={tiktok} className='pl-12 h-20' />
-          <img src={macquarie} className='pl-12 h-20' />
+          <img src={janeStreet} className='h-16' />
+          <img src={tiktok} className='pl-12 h-16' />
+          <img src={macquarie} className='pl-12 h-16' />
         </div>
       </div>
     </div>
