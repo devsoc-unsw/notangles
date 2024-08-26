47200ccd6256d4dadc050b2f646e8fd6d88fcf14
Dylan Zhang <z5478419@ad.unsw.edu.au>
2024-08-26 23:27:52 +1000

    sync from macbook

--- client/src/components/landingPage/LandingPage.tsx
+++ client/src/components/landingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection/heroSection";
 import SponsorsSection from "./SponsorsSection";
 
@@ -5,6 +6,7 @@ const LandingPage = () => {
   return (
     <div className="bg-blue-100 min-h-[3000px]">
       <HeroSection />
+      <FeaturesSection />
       <SponsorsSection />
     </div>
   );
