02c7c1759fbbbdc5190827276ef24966b268c91a
Jasmine Tran <z5394841@ad.unsw.edu.au>
2024-08-29 08:50:11 +1000

    search bar template for add a friend

--- client/src/components/sidebar/groupsSidebar/friends/AddAFriendTab.tsx
+++ client/src/components/sidebar/groupsSidebar/friends/AddAFriendTab.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Autocomplete, Box, TextField } from '@mui/material';
 import { User } from '../GroupsSidebar';
 import UserProfile from './UserProfile';
 import styled from '@emotion/styled';
@@ -9,22 +9,111 @@ const StyledFriendsListContainer = styled('div')`
   gap: 12px;
 `;
 
+interface UserSearchType {
+  zid: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  profileURL: string;
+}
+
+const get_all_users = (): UserSearchType[] => {
+  return [
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+    {
+      zid: 'z5555555',
+      firstname: 'Jasmine',
+      lastname: 'Tran',
+      email: 'raysbae@gmail.com',
+      profileURL: 'https://pbs.twimg.com/media/FysPI22WcAg9tfz.jpg',
+    },
+  ];
+};
+
 const AddAFriendTab: React.FC<{ user: User | undefined }> = ({ user }) => {
   if (!user) return;
-  console.log('user', user)
+  console.log('user', user);
 
   return (
-    <StyledFriendsListContainer>
-      {user.friends.map((friend: User, i) => (
-        <UserProfile
-          key={i}
-          firstname={friend.firstname}
-          lastname={friend.lastname}
-          email={friend.email}
-          profileURL={friend.profileURL}
-        />
-      ))}
-    </StyledFriendsListContainer>
+    <Autocomplete
+      id="user_search"
+      sx={{ width: '100%' }}
+      options={get_all_users()}
+      autoHighlight
+      getOptionLabel={(user) => `${user.firstname} ${user.lastname}`}
+      renderOption={(props, user) => {
+        const { key, ...optionProps } = props;
+        return (
+          <Box key={key} component="li" sx={{ '& > img': { borderRadius: 1, mr: 2, flexShrink: 0 } }} {...optionProps}>
+            <img loading="lazy" width="20" srcSet={user.profileURL} src={user.profileURL} alt="" />
+            {user.firstname} {user.lastname} ({user.zid})
+          </Box>
+        );
+      }}
+      renderInput={(params) => <TextField {...params} label="Search for users..." />}
+    />
   );
 };
 
