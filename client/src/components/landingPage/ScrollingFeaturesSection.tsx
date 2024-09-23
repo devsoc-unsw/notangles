9dd5356988cdd7a316b90b5367eea27bfb365abc
dylan <robouser51@gmail.com>
2024-09-24 00:11:43 +1000

    finished scrolling features section except for gifs

--- client/src/components/landingPage/ScrollingFeaturesSection.tsx
+++ client/src/components/landingPage/ScrollingFeaturesSection.tsx
@@ -1,13 +1,53 @@
-const ScrollingFeaturesSection = () => {
-  return (
-    <div className="flex justify-center items-center min-w-full min-h-[550px] bg-blue-500">
-      <div className="flex-col justify-center items-between text-5xl w-[800px] font-semibold text-neutral-50">
+interface FeatureItemProps {
+  number: string;
+  title: string;
+  description: string;
+}
+
+const FeatureItem: React.FC<FeatureItemProps> = ({ number, title, description }) => (
+  <div className="w-full h-[650px] flex">
+    <div className="bg-blue-500 w-1/2 flex flex-col items-center justify-center">
+      <div className="font-extrabold w-3/4 text-9xl">
+        <p>{number}</p>
       </div>
+      <div className="w-3/4 font-bold text-4xl mt-10 mb-6">
+        <p>{title}</p>
+      </div>
+      <div className="w-3/4">
+        <p>{description}</p>
+      </div>
+    </div>
+    <div className="w-1/2 bg-blue-100">
+
+    </div>
+  </div>
+);
+
+const ScrollingFeaturesSection = () => {
+  const features = [
+    {
+      number: '01',
+      title: 'Course Selector',
+      description: 'Search and select your desired course and get a general overview of each class, their times and locations, all before the registrations start!',
+    },
+    {
+      number: '02',
+      title: 'Plan Ahead',
+      description: 'Add any course classes or custom events to your calendar, effectively scheduling all your uni commitments!'
+    },
+    {
+      number: '03',
+      title: 'Auto Timetabling',
+      description: 'Struggling to make your ideal timetable? Put in your preferences and we’ll generate one for you!' },
+  ];
+
+  return (
+    <div className="mt-40 h-[1000px] flex-col text-blue-50">
+      {features.map((feature, index) => (
+        <FeatureItem key={index} {...feature} />
+      ))}
     </div>
   );
 };
 
 export default ScrollingFeaturesSection;
-
-
-
