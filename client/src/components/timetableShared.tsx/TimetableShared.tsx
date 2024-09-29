c5e0fc25a1a2920d43263dfca6445a22adb95658
Jasmine Tran <z5394841@ad.unsw.edu.au>
2024-09-29 12:42:17 +1000

    created untested code for setting the combined activities of group members and admins

--- client/src/components/timetableShared.tsx/TimetableShared.tsx
+++ client/src/components/timetableShared.tsx/TimetableShared.tsx
@@ -5,16 +5,20 @@ import { CourseContext } from '../../context/CourseContext';
 import { UserContext } from '../../context/UserContext';
 import { TimetableProps } from '../../interfaces/PropTypes';
 import Timetable from '../timetable/Timetable';
+import { CourseData, CreatedEvents, SelectedClasses } from '../../interfaces/Periods';
+import { User } from '../sidebar/UserAccount';
 
 const Container = styled('div')`
   margin-top: 12px;
 `;
 
 const TimetableShared: React.FC<TimetableProps> = ({ assignedColors, handleSelectClass }) => {
-  const { groups, groupsSidebarCollapsed } = useContext(UserContext);
+  const { groups, groupsSidebarCollapsed, user, selectedGroupIndex } = useContext(UserContext);
   if (groups.length === 0) return <></>;
+  const group = groups[selectedGroupIndex];
+  const userTimetable = user.timetables[0];
 
-  const { selectedCourses, setAssignedColors } = useContext(CourseContext);
+  const { setAssignedColors, setSelectedCourses, setSelectedClasses, setCreatedEvents } = useContext(CourseContext);
 
   const showPersonalTimetable = () => {
     ///////// TODO uncomment below when BE done, so that user.timetable works.
@@ -25,29 +29,48 @@ const TimetableShared: React.FC<TimetableProps> = ({ assignedColors, handleSelec
     // setAssignedColors(assignedColors);
   };
 
-  const getNewAssignedColours = () => {
+  // Helper function that combines users' courses, classes and created events, with no duplicates.
+  const combineUsersActivities = (users: User[]) => {
+    let newSelectedCourses: CourseData[] = [];
+    const newSelectedClasses: SelectedClasses = {};
+    const newCreatedEvents: CreatedEvents = {};
+
+    for (const user of users) {
+      newSelectedCourses = [...new Set([...newSelectedCourses, ...user.timetables[0].selectedCourses])];
+      Object.assign(newSelectedClasses, user.timetables[0].selectedClasses);
+      Object.assign(newCreatedEvents, user.timetables[0].createdEvents);
+    }
+
+    return { newSelectedCourses, newSelectedClasses, newCreatedEvents };
+  };
+
+  // Sets selectedCourses, selectedClasses and createdEvents as the combined activities of the group's members and admins.
+  const setSharedActivities = () => {
+    const mergedMembers = combineUsersActivities(group.members);
+    const mergedAdmins = combineUsersActivities(group.groupAdmins);
+    setSelectedCourses([...new Set([...mergedMembers.newSelectedCourses, ...mergedAdmins.newSelectedCourses])]);
+    setSelectedClasses(Object.assign(mergedMembers.newSelectedClasses, mergedAdmins.newSelectedClasses));
+    setCreatedEvents(Object.assign(mergedMembers.newCreatedEvents, mergedAdmins.newCreatedEvents));
+  };
+
+  const setSharedAssignedColours = () => {
     const newAssignedColours: Record<string, string> = {};
     // Friend's activities has grey colour of: '#D3D3D3'
-    // group.members.map((member) =>
-    //   member.timetables[0].selectedCourses.map((course) => {
-    //     newAssignedColours[course.code] = '#D3D3D3';
-    //   }),
-    // );
-
-    // Logged in user has unique colour of: #137786 Turquoise
-    selectedCourses.map((course) => {
-      newAssignedColours[course.code] = '#137786';
+    group.members.map((member) => {
+      member.timetables[0].selectedCourses.map((course) => {
+        newAssignedColours[course.code] = '#D3D3D3';
+      });
     });
-
-    setAssignedColors(newAssignedColours);
+    // Logged in user has unique colour of: #137786 Turquoise
+    userTimetable.selectedCourses.map((course) => {
+      newAssignedColours[course.code] = '#137786';
+    }),
+      setAssignedColors(newAssignedColours);
   };
 
   const showSharedTimetable = () => {
-    getNewAssignedColours();
-    // can't drag friend's events
-    // for ExpandedClassView, show friends only sharing the class.
-    // showing only first personal timetable
-    // group banner
+    setSharedActivities();
+    setSharedAssignedColours();
   };
 
   // update timetable shown when switching between Shared and Personal timetables.
