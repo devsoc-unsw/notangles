e7085aac465c80f91fff56d9a042d6c911021048
{jason4193} <{jason4193@gmail.com}>
2025-04-05 17:44:26 +1100

    Enhance color selection with themed styled buttons

--- client/src/components/controls/ColorOptions.tsx
+++ client/src/components/controls/ColorOptions.tsx
@@ -1,8 +1,10 @@
+import styled from '@emotion/styled';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
-import {IconButton, List, ListItem, Tooltip } from '@mui/material';
+import {IconButton, List, ListItem } from '@mui/material';
 import React from 'react';
 
+import { darkTheme, lightTheme } from '../../constants/theme';
 interface ColorOptionsProps {
   colors: string[];
   maxDefaultColors?: number;
@@ -11,6 +13,16 @@ interface ColorOptionsProps {
   onCustomColorSelect: () => void;
 }
 
+const StyledColorIconButton = styled(IconButton)<{ border: string; bgColor: string }>(({ border, bgColor }) => ({
+  backgroundColor: bgColor,
+  width: 40,
+  height: 40,
+  '&:hover': {
+    backgroundColor: bgColor,
+    border: `2px solid ${border}`,
+  }
+}));
+
 const ColorOptions: React.FC<ColorOptionsProps> = ({
   colors,
   maxDefaultColors = 4, // Default to 4 color options
@@ -18,27 +30,31 @@ const ColorOptions: React.FC<ColorOptionsProps> = ({
   onSelectColor,
   onCustomColorSelect
 }) => {
+  // Get the current theme as from local storage
+  const data = localStorage.getItem('data');
+  const parsedData = data ? JSON.parse(data) : null;
+  const theme = parsedData?.["isDarkMode"] ? darkTheme : lightTheme;
+
   return (
     <List sx={{ display: 'flex', flexDirection: 'row', gap: 1 }}>
       {colors.slice(0, maxDefaultColors).map((color) => (
         <ListItem key={color} disableGutters>
-          <Tooltip title={color}>
-          <IconButton
-            style={{ backgroundColor: color, width: 40, height: 40 }}
+          <StyledColorIconButton
+            border={theme.palette.secondary.main}
+            bgColor={color}
             onClick={() => onSelectColor(color)}
-          />
-          </Tooltip>
+
+        />
         </ListItem>
       ))}
       <ListItem disableGutters>
-      <Tooltip title="Custom Color">
-        <IconButton
-        style={{ backgroundColor: 'gray', width: 40, height: 40 }}
-        onClick={onCustomColorSelect}
+        <StyledColorIconButton
+          border={theme.palette.secondary.main}
+          bgColor={showCustomColorPicker ? theme.palette.secondary.dark : theme.palette.secondary.dark}
+          onClick={onCustomColorSelect}
         >
         {showCustomColorPicker ? <CloseIcon /> : <AddIcon />}
-        </IconButton>
-      </Tooltip>
+        </StyledColorIconButton>
       </ListItem>
     </List>
   );
