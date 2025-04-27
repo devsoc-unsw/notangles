3588b9516e2cbb3cb3231520db5f8fa697261acc
{jason4193} <{jason4193@gmail.com}>
2025-04-27 17:14:24 +1000

    Reverted the color-assign logic back to previous logic, random logic for furture use

--- client/src/hooks/useColorMapper.ts
+++ client/src/hooks/useColorMapper.ts
@@ -23,10 +23,14 @@ const useColorMapper = (courseCodes: string[], assignedColors: Record<string, st
     }
 
     if (!(course in newAssignedColors)) {
+      // Previous logic
+      color = colors.find((c) => !takenColors.has(c));
+      
+      // New logic (Apply in the future application)
       // Randomly assign a color that is not already taken
-      const availableColors = colors.filter((c) => !takenColors.has(c));
-      color = availableColors[Math.floor(Math.random() * availableColors.length)];
-      newAssignedColors[course] = color || defaultColor;
+      // const availableColors = colors.filter((c) => !takenColors.has(c));
+      // color = availableColors[Math.floor(Math.random() * availableColors.length)];
+      // newAssignedColors[course] = color || defaultColor;
     }
 
     if (color) {
