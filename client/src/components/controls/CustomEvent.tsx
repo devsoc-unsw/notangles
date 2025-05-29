64750dfa7c3b657a101e9aa30e20a46ed77dc6f1
Lucas <me@lhvy.dev>
2025-05-29 14:45:10 +1000

    Add project as an option for tutoring (#995)

--- client/src/components/controls/CustomEvent.tsx
+++ client/src/components/controls/CustomEvent.tsx
@@ -63,7 +63,7 @@ const CustomEvent: React.FC = () => {
 
   // Get the list of classes for the selected course code.
   useEffect(() => {
-    const tutoringActivities = ['Tutorial', 'Laboratory', 'Tutorial-Laboratory', 'Workshop', 'Seminar'];
+    const tutoringActivities = ['Tutorial', 'Laboratory', 'Tutorial-Laboratory', 'Workshop', 'Seminar', 'Project'];
     if (courseCode !== '') {
       getCourseInfo(term.substring(0, 2), courseCode, term!.substring(2), isConvertToLocalTimezone)
         .catch((err) => {
