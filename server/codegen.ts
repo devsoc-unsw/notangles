5c42d77138d68b84729fcdadfe94ba0a5ac79755
Lucas <me@lhvy.dev>
2025-05-29 16:45:24 +1000

    Setup server graphql client

---
+++ server/codegen.ts
@@ -0,0 +1,22 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://graphql.csesoc.app/v1/graphql',
+  documents: ['src/**/*.ts'],
+  ignoreNoDocuments: true,
+  generates: {
+    './src/generated/graphql/': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-graphql-request',
+      ],
+      preset: 'client',
+      config: {
+        documentMode: 'string',
+      },
+    },
+  },
+};
+
+export default config;
