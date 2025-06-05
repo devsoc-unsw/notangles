2c031c054bac67eaf6d4dd4ff4aa4eb3e5a4bd57
jason4193 <100593637+jason4193@users.noreply.github.com>
2025-06-05 19:09:27 +1000

    Migrate to new theme settings from previous settings (#991)
    * Created mitgrate function for the frontend to update local storage
    
    * Wrote the migration.sql script for update events colour on Database
    
    * Updated the frontend migrate function to update the colour of a createdEvent
    
    * Removed redudant logging
    
    * Update the migration file to run migration properly.

--- client/src/utils/storage.ts
+++ client/src/utils/storage.ts
@@ -2,6 +2,58 @@ import defaults from '../constants/defaults';
 
 const STORAGE_KEY = 'data';
 
+const MIGRATE_COLOR_MAP: Record<string, string> = {
+  '#137786': 'default-1',
+  '#a843a4': 'default-2',
+  '#134e86': 'default-3',
+  '#138652': 'default-4',
+  '#861313': 'default-5',
+  '#868413': 'default-6',
+  '#2e89ff': 'default-7',
+  '#3323ad': 'default-8',
+};
+
+const migrateTimetables = (timetables: Record<string, any>) => {
+  return Object.fromEntries(
+    Object.entries(timetables).map(([termKey, termValue]) => [termKey, termValue.map(migrateTimetable)]),
+  );
+};
+
+const migrateTimetable = (timetable: any) => {
+  return {
+    ...timetable,
+    createdEvents: migrateCreatedEvents(timetable.createdEvents),
+    assignedColors: migrateAssignedColors(timetable.assignedColors),
+  };
+};
+
+const migrateCreatedEvents = (createdEvents: Record<string, any>) => {
+  return Object.fromEntries(
+    Object.entries(createdEvents).map(([eventKey, eventValue]) => [
+      eventKey,
+      {
+        ...eventValue,
+        event: {
+          ...eventValue.event,
+          color:
+            eventValue.event.color in MIGRATE_COLOR_MAP
+              ? MIGRATE_COLOR_MAP[eventValue.event.color]
+              : eventValue.event.color,
+        },
+      },
+    ]),
+  );
+};
+
+const migrateAssignedColors = (assignedColors: Record<string, string>) => {
+  return Object.fromEntries(
+    Object.entries(assignedColors).map(([key, color]) => [
+      key,
+      color in MIGRATE_COLOR_MAP ? MIGRATE_COLOR_MAP[color] : color,
+    ]),
+  );
+};
+
 const storage = {
   get: (key: string): any => {
     const data: Record<string, any> = storage.load();
@@ -28,6 +80,8 @@ const storage = {
 
     if (localStorage[STORAGE_KEY]) {
       data = JSON.parse(localStorage[STORAGE_KEY]);
+      // migrate old data format to new format
+      data = storage.migrate(data);
     } else {
       storage.save(data);
     }
@@ -37,6 +91,20 @@ const storage = {
 
   save: (data: Record<string, any>) => {
     localStorage[STORAGE_KEY] = JSON.stringify(data);
+  },
+
+  migrate: (data: Record<string, any>) => {
+    // only do this if version does not exist
+    if (data.version !== 1 || data.version == null) {
+      let migrated = {
+        ...data,
+        version: 1,
+        timetables: migrateTimetables(data.timetables),
+      };
+      storage.save(migrated);
+      return migrated;
+    }
+    return data;
   },
 };
 
