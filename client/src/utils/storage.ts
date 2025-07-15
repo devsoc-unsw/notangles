d842d11a0cf6282e746205ebdce963d316bbca7a
Lucas <me@lhvy.dev>
2025-07-15 17:12:58 +1000

    More aggressively catch corrupted local storage

--- client/src/utils/storage.ts
+++ client/src/utils/storage.ts
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
 import defaults from '../constants/defaults';
 import migrateThemes from './migrations/colourTheme';
 import migratePrimaryTimetables from './migrations/primaryTimetables';
 import { createDefaultTimetable } from './timetableHelpers';
 
 const STORAGE_KEY = 'data';
+
+// Something is not playing nice and causing local storage to corrupt across signing in/out
+// We are in the process of migrating data to the backend, so this fix should catch people
+// for now and prevent them from seeing a blank page.
+const hasRunArrayCheck = useRef(false);
 
 const storage = {
   get: (key: string): any => {
@@ -64,7 +70,7 @@ const storage = {
     }
 
     // Un-break an existing issue with migrated data
-    if (migrated.version == null || migrated.version < 2) {
+    if (!hasRunArrayCheck.current) {
       Object.entries(migrated.timetables).forEach(([term, timetables]) => {
         if (
           !Array.isArray(timetables) ||
@@ -73,6 +79,8 @@ const storage = {
           migrated.timetables[term] = createDefaultTimetable('');
         }
       });
+
+      hasRunArrayCheck.current = true;
     }
 
     // only do this if version does not exist
