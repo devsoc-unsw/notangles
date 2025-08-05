045ce3ed8c026d9ad2df9d1f52ce145ec9a13683
jason4193 <z5391104@ad.unsw.edu.au>
2025-08-05 15:21:37 +1000

    Migrate JS fetch to Axios

--- client/src/api/user/routes.ts
+++ client/src/api/user/routes.ts
@@ -1,32 +1,23 @@
 import { API_URL } from '../config';
-import { UserSettings } from '../../interfaces/User';
+import { UserInfo, UserSettings } from '../../interfaces/User';
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: API_URL.server,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getUserProfile = async (): Promise<UserInfo> => {
+  return (await apiClient.get('/user/profile')).data;
+};
 
 export const getUserSettings = async (): Promise<UserSettings> => {
-  try {
-    const res = await fetch(`${API_URL.server}/user/settings`, {
-      method: 'GET',
-      credentials: 'include',
-    });
-    if (res.status !== 200) {
-      throw new Error('Could not fetch user settings');
-    }
-    return await res.json();
-  } catch (error) {
-    throw new Error('Error fetching user settings');
-  }
+  return (await apiClient.get('/user/settings')).data;
 };
 
 export const setUserSettings = async (settings: Partial<UserSettings>): Promise<void> => {
-  try {
-    const res = await fetch(`${API_URL.server}/user/settings`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify(settings),
-    });
-  } catch (error) {
-    throw new Error('Could not set user settings');
-  }
+  await apiClient.post('/user/settings', settings);
 };
