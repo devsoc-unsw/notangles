b6c7822aec828f2afb51391d8f5bdd43de1c2cfe
Lucas <me@lhvy.dev>
2025-09-07 22:29:10 +1000

    Refactor and add logout route

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { AuthProvider } from 'src/generated/prisma/enums';
+import { UserService } from 'src/user/user.service';
 
 export interface AuthenticatedRequest extends Request {
   user: {
@@ -14,6 +15,8 @@ export interface AuthenticatedRequest extends Request {
 
 @Controller('auth')
 export class AuthController {
+  constructor(private userService: UserService) {}
+
   @Get('login/github')
   @UseGuards(AuthGuard('github'))
   githubLogin() {}
@@ -29,38 +32,52 @@ export class AuthController {
   @Get('callback/github')
   @UseGuards(AuthGuard('github'))
   githubCallback(@Res() res: Response) {
-    res.redirect(
-      (process.env.NODE_ENV === 'dev' ? `http://` : `https://`) +
-        `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}/home`,
-    );
+    this.redirectAfterAuth(res);
   }
 
   @Get('callback/google')
   @UseGuards(AuthGuard('google'))
   googleCallback(@Res() res: Response) {
-    res.redirect(
-      (process.env.NODE_ENV === 'dev' ? `http://` : `https://`) +
-        `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}/home`,
-    );
+    this.redirectAfterAuth(res);
   }
 
   @Get('callback/devsoc')
   @UseGuards(AuthGuard('oidc'))
   callback(@Res() res: Response) {
-    res.redirect(
-      (process.env.NODE_ENV === 'dev' ? `http://` : `https://`) +
-        `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}/home`,
-    );
+    this.redirectAfterAuth(res);
   }
 
   @Get('login/guest')
   @UseGuards(AuthGuard('guest'))
   guest(@Res() res: Response) {
+    this.redirectAfterAuth(res);
+  }
+
+  @Get('logout')
+  async logout(@Req() req: Request, @Res() res: Response) {
+    let userId: string | undefined = undefined;
+    if (req.isAuthenticated() && (req as AuthenticatedRequest).user.isGuest) {
+      userId = (res.req as AuthenticatedRequest).user.id;
+    }
+
+    res.clearCookie('connect.sid');
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    req.logout((err) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      req.session?.destroy((err) => {
+        res.send();
+      });
+    });
+
+    if (userId) {
+      await this.userService.deleteUser(userId);
+    }
+  }
+
+  private redirectAfterAuth(res: Response) {
     res.redirect(
       (process.env.NODE_ENV === 'dev' ? `http://` : `https://`) +
         `${process.env.CLIENT_HOST_NAME}:${process.env.CLIENT_HOST_PORT}/home`,
     );
   }
-
-  // TODO: Add logout (and deleting guest users)
 }
