31479afc54be6f70e124e39de6b586c0476261b2
Mark Tran <29350857+marktran2@users.noreply.github.com>
2025-09-17 11:12:38 +1000

    reverting auth guard lol

--- client/src/components/login/AuthGuard.tsx
+++ client/src/components/login/AuthGuard.tsx
@@ -1,13 +1,13 @@
 import type { JSX } from 'react';
-// import { Navigate } from 'react-router';
+import { Navigate } from 'react-router';
 
-// import { useAuth } from '../../hooks/useAuth';
-// import PageLoading from '../pageLoading/PageLoading';
+import { useAuth } from '../../hooks/useAuth';
+import PageLoading from '../pageLoading/PageLoading';
 
 export function AuthGuard({ children }: { children: JSX.Element }) {
-  // const { loading, loggedIn } = useAuth();
+  const { loading, loggedIn } = useAuth();
 
-  // if (loading) return <PageLoading />;
-  // if (!loggedIn) return <Navigate to="/" replace />;
+  if (loading) return <PageLoading />;
+  if (!loggedIn) return <Navigate to="/" replace />;
   return children;
 }
