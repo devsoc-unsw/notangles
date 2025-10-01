0269e8cde638f08448e6f8c38852092a10a20513
rfquin <rfquinny@gmail.com>
2025-10-01 18:06:28 +1000

    Oop I made an error - please don't kick me off devsoc

--- server/src/friendship/friendship.service.ts
+++ server/src/friendship/friendship.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Friendship } from './types';
 
 @Injectable({})
 export class FriendshipService {
