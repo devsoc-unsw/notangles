270677073ca71268d64835a96ecdccef132b97c1
Tet Nay Lin <tetnaylin168@gmail.com>
2025-10-08 18:28:24 +1100

    Integrate pfp upload implementation

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,9 +3,20 @@ import { GraphqlService } from 'src/graphql/graphql.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MinioModule } from 'nestjs-minio-client';
 
 @Module({
+  imports: [
+    MinioModule.register({
+      endPoint: process.env.MINIO_ENDPOINT ?? 'localhost',
+      port: 9000,
+      useSSL: false,
+      accessKey: process.env.MINIO_ACCESSKEY!,
+      secretKey: process.env.MINIO_SECRETKEY!,
+    }),
+  ],
   providers: [UserService, PrismaService, GraphqlService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserInfo, UserSettings } from './types';
+import { MinioService } from 'nestjs-minio-client';
+import * as crypto from 'crypto';
 
 @Injectable({})
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly minio: MinioService,
+  ) {}
 
   async deleteUser(userId: string): Promise<void> {
     await this.prisma.user.delete({
@@ -29,16 +34,62 @@ export class UserService {
     };
   }
 
+  async uploadImage(image: string): Promise<{ url: string }> {
+    // Basic base64 string validation (does not check for file type)
+    const base64regex =
+      /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
+    if (!base64regex.test(image)) {
+      throw new Error('Invalid string');
+    }
+
+    const temp_filename = Date.now().toString();
+    const hash = crypto.createHash('md5');
+    hash.update(temp_filename);
+    const hashedFileName = hash.digest('hex');
+    const filename = hashedFileName;
+
+    const buffer = Buffer.from(image, 'base64');
+
+    const bucketName = process.env.MINIO_BUCKET_NAME;
+
+    try {
+      await this.minio.client.putObject(bucketName!, filename, buffer);
+    } catch (err) {
+      throw new Error('Error uploading file to MinIO: ' + err);
+    }
+
+    return {
+      url: `${process.env.MINIO_PUBLIC_URL}/${bucketName}/${filename}`,
+    };
+  }
+
   async setProfilePicture(
     userId: string,
     profilePictureUrl: string,
   ): Promise<void> {
+    if (!profilePictureUrl) {
+      // null is passed in, set the field to null (client will show the default image)
+      await this.prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          profilePictureUrl: null,
+        },
+      });
+      return;
+    }
+
+    const minioImageLink = await this.uploadImage(profilePictureUrl);
+
     await this.prisma.user.update({
       where: {
         id: userId,
       },
       data: {
-        profilePictureUrl,
+        // consider for empty string being passed in -> set to default image
+        profilePictureUrl: minioImageLink.url,
       },
     });
   }
