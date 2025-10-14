ba006fe57827163016f99c22541916b91d15aaeb
louis <louislim2005@gmail.com>
2025-10-15 00:39:55 +1100

    Added pending friend invites badge with hardcoded invite values. Changed friends icon to a group icon instead.

--- client/src/components/sidebar/AddFriendsButton.tsx
+++ client/src/components/sidebar/AddFriendsButton.tsx
@@ -6,6 +6,17 @@ import { useContext, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 import AddFriendsMenu from './AddFriendsMenu';
 import CustomModal from './CustomModel';
+import PendingInvitesBadge from './PendingInvitesBadge';
+
+const BadgeWrapper = styled('div')`
+  position: relative;
+`;
+
+const BadgePositioner = styled('div')`
+  position: absolute;
+  bottom: 29px;
+  right: 3px;
+`;
 
 const StyledAddButton = styled(IconButton)`
   display: flex;
@@ -17,11 +28,15 @@ const StyledAddButton = styled(IconButton)`
   color: ${({ theme }) => theme.palette.text.primary};
   border: 1px solid;
   border-color: ${({ theme }) => theme.palette.primary.main};
+  width: 100%;
 `;
 
 const StyledAddIcon = styled(AddCircle)`
   color: ${({ theme }) => theme.palette.primary.main};
 `;
+
+// Hardcoded values to be replaced
+const friendInvites = 2;
 
 interface AddFriendsButtonProps {
   friendsListOpen: boolean;
@@ -39,12 +54,18 @@ const AddFriendsButton = ({ friendsListOpen }: AddFriendsButtonProps) => {
 
   return (
     <>
-      <StyledAddButton onClick={toggleModal}>
-        {!sidebarCollapsed && <Typography fontSize={15}>Add Friends</Typography>}
-        <Tooltip title="Add Friends" placement="right">
-          <StyledAddIcon />
-        </Tooltip>
-      </StyledAddButton>
+      <BadgeWrapper>
+        <StyledAddButton onClick={toggleModal}>
+          {!sidebarCollapsed && <Typography fontSize={15}>Add Friends</Typography>}
+            <Tooltip title="Add Friends" placement="right">
+              <StyledAddIcon />
+            </Tooltip>
+        </StyledAddButton>
+        
+        <BadgePositioner>
+          <PendingInvitesBadge count={friendInvites} />
+        </BadgePositioner>
+      </BadgeWrapper>
       <CustomModal
         description="Add Friends"
         content={<AddFriendsMenu />}
--- client/src/components/sidebar/FriendsButton.tsx
+++ client/src/components/sidebar/FriendsButton.tsx
@@ -1,10 +1,12 @@
 import { ArrowDropDown, ArrowDropUp, Person } from '@mui/icons-material';
+import GroupIcon from '@mui/icons-material/Group';
 import { IconButton, Tooltip, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/system';
 import { useContext, useMemo } from 'react';
 
 import { AppContext } from '../../context/AppContext';
+import PendingInvitesBadge from './PendingInvitesBadge';
 
 interface FriendsButtonProps {
   friendsListOpen: boolean;
@@ -32,6 +34,22 @@ const IndividualComponentTypography = styled(Typography)`
   font-size: 16px;
 `;
 
+const RightContainer = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  position: relative;
+`;
+
+const BadgePositioner = styled('div', { shouldForwardProp: (prop) => prop !== 'sidebarCollapsed' })<{
+  sidebarCollapsed: boolean;
+}>`
+  display: ${({ sidebarCollapsed }) => (sidebarCollapsed ? '' : 'flex' )};
+  position: ${({ sidebarCollapsed }) => (sidebarCollapsed ? 'absolute' : 'static' )};
+  bottom: -2px;
+  right: 14px;
+`;
+
 // TODO: Repurpose using Sunny's design for friends
 const FriendsButton = ({ friendsListOpen, handleFriendsListToggle }: FriendsButtonProps) => {
   const { sidebarCollapsed } = useContext(AppContext);
@@ -39,15 +57,22 @@ const FriendsButton = ({ friendsListOpen, handleFriendsListToggle }: FriendsButt
     if (sidebarCollapsed) return null;
     return friendsListOpen ? <ArrowDropUp /> : <ArrowDropDown />;
   }, [sidebarCollapsed, friendsListOpen]);
+  const friendInvites = 2; // Hardcoded 
 
   return (
     <Tooltip title="Friends" placement="right">
       <StyledFriendsButton color="inherit" isSelected={false} onClick={handleFriendsListToggle}>
         <StyledFriendsContainer>
-          <Person />
+          <GroupIcon />
           <IndividualComponentTypography>{sidebarCollapsed ? '' : 'Friends'}</IndividualComponentTypography>
         </StyledFriendsContainer>
-        {friendToggleArrow}
+        <RightContainer>
+          <BadgePositioner sidebarCollapsed={sidebarCollapsed}>
+            <PendingInvitesBadge count={friendInvites} showBadge={!friendsListOpen}/>
+          </BadgePositioner>
+          {friendToggleArrow}
+        </RightContainer>
+        
       </StyledFriendsButton>
     </Tooltip>
   );
