803d80844f7e14bbecfcdf37a9c5834d22e1f93d
fox-58 <sophie@foxy.net.au>
2025-10-20 18:21:57 +1100

    remove console log

--- server/src/timetable/timetable.controller.ts
+++ server/src/timetable/timetable.controller.ts
@@ -271,7 +271,6 @@ export class TimetableController {
     @Body() body: EventParametersDto,
   ) {
     try {
-      console.log(req.user.id, eventId, body);
       await this.timetableService.updateEvent(req.user.id, eventId, body);
     } catch (error) {
       if (error instanceof HttpException) {
