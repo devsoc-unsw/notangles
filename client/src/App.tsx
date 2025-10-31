23145288decd91f7759d0c5ee1a4903e2ca7e1cc
Lucas <me@lhvy.dev>
2025-10-31 14:48:20 +1100

    Update new timetable promotional popup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -647,7 +647,7 @@ const App: React.FC = () => {
                   <SubcomPromotion />
                   <PromotionPopup
                     imgSrc={T3SelectGif}
-                    title="Next term's timetable has been released! 🎉"
+                    title="The 2026 Term 1 timetable has been released! 🎉"
                     subTitle="Organise, plan and schedule with newly released timetable"
                     bullets={[
                       {
