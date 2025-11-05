3ca1afa2480a80a43e6ac7300503de60f518c047
Tet Nay Lin <tetnaylin168@gmail.com>
2025-11-06 02:01:42 +1100

    Remove file-upload

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import config from './config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { FileUploadModule } from './file-upload/file-upload.module';
 import { TimetableModule } from './timetable/timetable.module';
 
 @Module({
@@ -18,7 +17,6 @@ import { TimetableModule } from './timetable/timetable.module';
     UserModule,
     TimetableModule,
     AuthModule,
-    FileUploadModule,
   ],
   controllers: [AppController],
   providers: [AppService],
