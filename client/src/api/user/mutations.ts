38f72e3f86781fd2cd461d05867d9335f68bd94f
louis <louislim2005@gmail.com>
2025-11-05 19:14:15 +1100

    Added mutations for friendship controllers using POST endpoints

--- client/src/api/user/mutations.ts
+++ client/src/api/user/mutations.ts
@@ -1,7 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 
 import type { UserSettings } from '../../interfaces/User';
-import { setUserSettings } from './routes';
+import {
+  setUserSettings,
+  createFriendRequest,
+  cancelFriendRequest,
+  acceptFriendRequest,
+  rejectFriendRequest,
+  removeFriend,
+} from './routes';
 
 export const useSetUserSettings = () =>
   useMutation({
@@ -10,3 +17,44 @@ export const useSetUserSettings = () =>
       invalidatesQuery: ['settings'],
     },
   }).mutate;
+
+export const useCreateFriendRequest = () =>
+  useMutation({
+    mutationFn: (requesteeCode: string) => createFriendRequest(requesteeCode),
+    meta: {
+      invalidatesQuery: ['friends', 'friendRequests'],
+    },
+  }).mutate;
+
+export const useCancelFriendRequest = () =>
+  useMutation({
+    mutationFn: (requesteeCode: string) => cancelFriendRequest(requesteeCode),
+    meta: {
+      invalidatesQuery: ['friendRequests'],
+    },
+  }).mutate;
+
+export const useAcceptFriendRequest = () =>
+  useMutation({
+    mutationFn: (requestorCode: string) => acceptFriendRequest(requestorCode),
+    meta: {
+      invalidatesQuery: ['friends', 'friendRequests'],
+    },
+  }).mutate;
+
+export const useRejectFriendRequest = () =>
+  useMutation({
+    mutationFn: (requestorCode: string) => rejectFriendRequest(requestorCode),
+    meta: {
+      invalidatesQuery: ['friendRequests'],
+    },
+  }).mutate;
+
+export const useRemoveFriend = () =>
+  useMutation({
+    mutationFn: (otherCode: string) => removeFriend(otherCode),
+    meta: {
+      invalidatesQuery: ['friends'],
+    },
+  }).mutate;
+  
