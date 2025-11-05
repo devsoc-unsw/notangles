594343352db65123e19102accb37b09df4c00cc7
louis <louislim2005@gmail.com>
2025-11-05 19:17:23 +1100

    Added queries for friendship controllers using GET endpoints

--- client/src/api/user/queries.ts
+++ client/src/api/user/queries.ts
@@ -1,9 +1,23 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 
-import { getUserSettings } from './routes';
+import { getFriends, getFriendRequests, getUserSettings } from './routes';
 
 export const useGetUserSettingsQuery = () =>
   useSuspenseQuery({
     queryKey: ['settings'],
     queryFn: getUserSettings,
   }).data;
+
+export const useFriendsQuery = () => {
+  return useQuery({
+    queryKey: ['friends'],
+    queryFn: getFriends,
+  });
+};
+
+export const useFriendRequestsQuery = () => {
+  return useQuery({
+    queryKey: ['friendRequests'],
+    queryFn: getFriendRequests,
+  });
+};
