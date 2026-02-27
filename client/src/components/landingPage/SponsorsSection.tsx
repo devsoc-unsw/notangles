cb950c2ef4d24afc564a342e0508258b983eb381
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2026-02-27 14:04:12 +1100

    feat: improve homepage spons layout

--- client/src/components/landingPage/SponsorsSection.tsx
+++ client/src/components/landingPage/SponsorsSection.tsx
@@ -16,26 +16,34 @@ const SponsorsSection = () => {
   return (
     <div className="flex flex-col h-auto rounded-xl w-full items-center justify-center py-16">
       <p className="text-center md:text-3xl text-4xl font-bold mb-16 md:mb-8">Brought to you by</p>
-      <ul className="grid grid-cols-1 md:grid-cols-[repeat(4,_minmax(100px,_1fr))] gap-y-8 place-items-center">
-        <li>
+      <ul className="grid grid-cols-1 md:grid-cols-4 gap-y-8 place-items-center gap-x-4">
+        <li className="w-full flex justify-center">
           <img src={hrt} alt="Hudson River Trading" className="h-12" />
         </li>
-        <li>
+        <li className="w-full flex justify-center">
           <img src={janeStreet} alt="Jane Street" className="h-11" />
         </li>
-        <li>
+        <li className="w-full flex justify-center">
           <img src={theTradeDesk} alt="theTradeDesk" className="h-9" />
         </li>
-        <li>
+        <li className="w-full flex justify-center">
           <img src={lyra} alt="Lyra" className="h-13" />
         </li>
-        <li className="md:col-span-4 grid grid-cols-2 md:grid-cols-4 place-items-center gap-y-8">
-          <img src={airwallex} alt="Airwallex" className="h-7" />
-          <img src={arista} alt="Arista" className="h-6" />
-          <img src={atlassian} alt="Atlassian" className="h-6" />
-          <img src={qrt} alt="QRT" className="h-6" />
+        <li className="md:col-span-4 grid grid-cols-2 md:grid-cols-4 place-items-center gap-y-8 gap-x-4">
+          <div className="w-full flex justify-center">
+            <img src={airwallex} alt="Airwallex" className="h-5 md:h-6" />
+          </div>
+          <div className="w-full flex justify-center">
+            <img src={arista} alt="Arista" className="h-5 md:h-6" />
+          </div>
+          <div className="w-full flex justify-center">
+            <img src={atlassian} alt="Atlassian" className="h-5 md:h-6" />
+          </div>
+          <div className="w-full flex justify-center">
+            <img src={qrt} alt="QRT" className="h-6" />
+          </div>
         </li>
-        <li className="md:col-span-4 grid grid-cols-3 md:grid-cols-3 place-items-center gap-y-8">
+        <li className="md:col-span-4 grid grid-cols-3 md:grid-cols- place-items-center gap-y-6">
           <img src={citadel} alt="Citadel Securities" className="h-8 invert" />
           <img src={imc} alt="IMC Trading" className="h-7 invert" />
           <img src={januaryCapital} alt="January Capital" className="h-7" />
