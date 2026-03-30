7332622b092426928b5922e69064f3e92b10091e
Lucas <me@lhvy.dev>
2026-03-30 12:20:26 +1100

    Fix missing postgres url in CI

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,9 +1,9 @@
 import 'dotenv-expand/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('POSTGRES_URL'),
+    url: process.env.POSTGRES_URL!,
   },
 });
