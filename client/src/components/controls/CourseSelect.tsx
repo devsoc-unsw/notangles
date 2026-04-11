a1110480db68803d53811649b926331b607955af
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2026-04-11 19:45:43 +1000

    fix: show no options component again without notfound impl

--- client/src/components/controls/CourseSelect.tsx
+++ client/src/components/controls/CourseSelect.tsx
@@ -400,7 +400,7 @@ const CourseSelect: React.FC<CourseSelectProps> = ({ assignedColors, handleSelec
       const paddingTop = 0;
 
       const visibleCount = Math.min(itemCount, maxResultsVisible);
-      const height = Array.from({ length: visibleCount }).reduce((sum: number, _, i) => sum + getItemSize(i), 0);
+      const height = Array.from({ length: visibleCount }).reduce((sum, _, i) => sum + getItemSize(i), 0);
 
       const Row: React.FC<ListChildComponentProps> = ({ data, index, style }) =>
         React.cloneElement(data[index], {
@@ -468,7 +468,7 @@ const CourseSelect: React.FC<CourseSelectProps> = ({ assignedColors, handleSelec
       <NoOptionsText variant="body2">
         Can&apos;t find <strong>{inputValue.trim().toUpperCase()}</strong>?
       </NoOptionsText>
-      <HelperLink onClick={handleHelperClick} underline="hover" color="primary">
+      <HelperLink onClick={handleHelperClick} underline="hover" color={theme.palette.primary.light}>
         Make sure you&apos;ve picked the right term by using the &apos;Select term&apos; dropdown to the left or
         clicking here.
       </HelperLink>
@@ -486,7 +486,7 @@ const CourseSelect: React.FC<CourseSelectProps> = ({ assignedColors, handleSelec
         disableListWrap
         selectOnFocus={false}
         options={mergedOptions}
-        noOptionsText={courseNotFound ? <NoOptions /> : 'No Results'}
+        noOptionsText={'No Results'}
         value={selectedValue}
         onChange={onChange}
         inputValue={inputValue}
@@ -497,9 +497,11 @@ const CourseSelect: React.FC<CourseSelectProps> = ({ assignedColors, handleSelec
         filterOptions={(o) => o}
         ListboxComponent={ListboxComponent}
         isOptionEqualToValue={(option, value) => option.code === value.code && option.career === value.career}
-        renderOption={(props, option, { selected }) => {
+        renderOption={(props, option, { index }) => {
           const { key, ...rest } = props;
-          return (
+          return courseNotFound && index === 0 ? (
+            <NoOptions key={key} />
+          ) : (
             <li key={key} {...rest}>
               <StyledOption>
                 <StyledIcon>
