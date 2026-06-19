8d2b5b47044bf4fc508212991dfa9b2060de0d79
Lyn Duong <z5585295@ad.unsw.edu.au>
2026-06-19 14:48:20 +1000

    added get free rooms function to freeroom service

--- server/src/freeroom/freeroom.module.ts
+++ server/src/freeroom/freeroom.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FreeroomController } from './freeroom.controller';
 import { FreeroomService } from './freeroom.service';
+import { GraphqlService } from 'src/graphql/graphql.service';
 
 @Module({
-  providers: [FreeroomService, PrismaService],
+  providers: [FreeroomService, PrismaService, GraphqlService],
   controllers: [FreeroomController],
 })
-export class FriendshipModule {}
+export class FreeroomModule {}
