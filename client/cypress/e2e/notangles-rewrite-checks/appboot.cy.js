0f0d5ce3cfe3551f32d2bc940bab340c36cbac73
Nathaniel Tan <nathanclw53@gmail.com>
2026-07-31 14:11:46 +1000

    added testing documentation

--- client/cypress/e2e/notangles-rewrite-checks/appboot.cy.js
+++ client/cypress/e2e/notangles-rewrite-checks/appboot.cy.js
@@ -7,4 +7,13 @@ describe("app booting", () => {
     cy.get("body").should("be.visible");
     cy.title().should("not.be.empty");
   });
+
+  it("validates the landing page regardless if profile fetching fails", () => {
+    cy.intercept("GET", "**/api/user/profile").as("profile");
+    cy.visit("/");
+    cy.wait("@profile").its("response.statusCode").should("be.oneOf", [401, 403]);
+    cy.contains("Get Started").should("be.visible");
+  });
+
+  
 });
